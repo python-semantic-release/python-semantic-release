57f4c2bdbfe88f6318c87bf6b2fbf851bc9f5a90
Rolf Erik Lekang <me@rolflekang.com>
2015-07-27 11:53:17 +0200

    :sparkles: Add project structure

---
+++ setup.py
@@ -0,0 +1,36 @@
+import re
+from setuptools import find_packages, setup
+
+
+def _read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst', format='markdown')
+    except Exception:
+        return None
+
+version = ''
+with open('semantic_release/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+setup(
+    name='python-semantic-release',
+    version=version,
+    url='http://github.com/relekang/python-semantic-release',
+    author='Rolf Erik Lekang',
+    author_email='me@rolflekang.com',
+    description='Automatic semantic versioning for python projects',
+    long_description=_read_long_description(),
+    packages=find_packages(exclude='tests'),
+    license='MIT',
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
