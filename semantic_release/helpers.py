a2ad75b8dac515d1bbc49c32257c62a7da59e2e1
Rolf Erik Lekang <me@rolflekang.com>
2015-07-27 13:57:54 +0200

    :sparkles: Implement setting of new version

--- semantic_release/helpers.py
+++ semantic_release/helpers.py
@@ -1,4 +1,6 @@
 import configparser
+import os
+import re
 import semver
 from invoke import run
 
@@ -17,7 +19,20 @@ def get_new_version(current_version, level_bump):
     return getattr(semver, 'bump_{0}'.format(level_bump))(current_version)
 
 
-def set_new_version(current_version):
+def set_new_version(new_version):
+    filename, variable = load_config().get('version_variable').split(':')
+    variable = variable.strip()
+    with open(filename, mode='r') as fr:
+        content = fr.read()
+
+    content = re.sub(
+        r'{} ?= ?["\']\d+\.\d+(?:\.\d+)?["\']'.format(variable),
+        '{} = \'{}\''.format(variable, new_version),
+        content
+    )
+
+    with open(filename, mode='w') as fw:
+        fw.write(content)
     return True
 
 
