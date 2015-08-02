1e47f1c6c68b5a74515f2f86275e3c749c999e1c
Rolf Erik Lekang <me@rolflekang.com>
2015-08-02 19:19:39 +0200

    Fix docstring in setup_hook()

--- semantic_release/__init__.py
+++ semantic_release/__init__.py
@@ -4,6 +4,7 @@ __version__ = '0.6.0'
 def setup_hook(argv):
     """
     A hook to be used in setup.py to enable `python setup.py publish`.
+
     :param argv: sys.argv
     """
     if len(argv) > 1 and argv[1] in ['version', 'publish']:
