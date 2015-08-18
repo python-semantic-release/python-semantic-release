c448d9b056d48d607a8bf7d38b8cf8a8ba038ca6
Rolf Erik Lekang <me@rolflekang.com>
2015-08-18 19:52:32 +0200

    refactor(history): Move evaluate_version_bump

--- semantic_release/history/__init__.py
+++ semantic_release/history/__init__.py
@@ -4,48 +4,7 @@ import semver
 from invoke import run
 
 from ..settings import config
-from ..vcs_helpers import get_commit_log
-
-LEVELS = {
-    1: 'patch',
-    2: 'minor',
-    3: 'major',
-}
-
-
-def evaluate_version_bump(current_version, force=None):
-    """
-    Reads git log since last release to find out if should be a major, minor or patch release.
-
-    :param current_version: A string with the current version number.
-    :param force: A string with the bump level that should be forced.
-    :return: A string with either major, minor or patch if there should be a release. If no release
-             is necessary None will be returned.
-    """
-    if force:
-        return force
-
-    bump = None
-
-    changes = []
-    commit_count = 0
-
-    for commit_message in get_commit_log():
-        if current_version in commit_message:
-            break
-        if config.get('semantic_release', 'major_tag') in commit_message:
-            changes.append(3)
-        elif config.get('semantic_release', 'minor_tag') in commit_message:
-            changes.append(2)
-        elif config.get('semantic_release', 'patch_tag') in commit_message:
-            changes.append(1)
-        commit_count += 1
-
-    if len(changes):
-        bump = LEVELS[max(changes)]
-    if config.getboolean('semantic_release', 'patch_without_tag') and commit_count:
-        bump = 'patch'
-    return bump
+from .logs import evaluate_version_bump
 
 
 def get_current_version():
--- tests/test_history.py
+++ tests/test_history.py
@@ -18,21 +18,21 @@ MAJOR_LAST_RELEASE_MINOR_AFTER = [MINOR, '1.1.0', MAJOR]
 
 class EvaluateVersionBumpTest(TestCase):
     def test_major(self):
-        with mock.patch('semantic_release.history.get_commit_log',
+        with mock.patch('semantic_release.history.logs.get_commit_log',
                         lambda: ALL_KINDS_OF_COMMIT_MESSAGES):
             self.assertEqual(evaluate_version_bump('0.0.0'), 'major')
 
     def test_minor(self):
-        with mock.patch('semantic_release.history.get_commit_log',
+        with mock.patch('semantic_release.history.logs.get_commit_log',
                         lambda: MINOR_AND_PATCH_COMMIT_MESSAGES):
             self.assertEqual(evaluate_version_bump('0.0.0'), 'minor')
 
     def test_patch(self):
-        with mock.patch('semantic_release.history.get_commit_log', lambda: PATCH_COMMIT_MESSAGES):
+        with mock.patch('semantic_release.history.logs.get_commit_log', lambda: PATCH_COMMIT_MESSAGES):
             self.assertEqual(evaluate_version_bump('0.0.0'), 'patch')
 
     def test_nothing_if_no_tag(self):
-        with mock.patch('semantic_release.history.get_commit_log', lambda: ['', '...']):
+        with mock.patch('semantic_release.history.logs.get_commit_log', lambda: ['', '...']):
             self.assertIsNone(evaluate_version_bump('0.0.0'))
 
     def test_force(self):
@@ -41,21 +41,21 @@ class EvaluateVersionBumpTest(TestCase):
         self.assertEqual(evaluate_version_bump('0.0.0', 'patch'), 'patch')
 
     def test_should_account_for_commits_earlier_than_last_commit(self):
-        with mock.patch('semantic_release.history.get_commit_log',
+        with mock.patch('semantic_release.history.logs.get_commit_log',
                         lambda: MAJOR_LAST_RELEASE_MINOR_AFTER):
             self.assertEqual(evaluate_version_bump('1.1.0'), 'minor')
 
     @mock.patch('semantic_release.history.config.getboolean', lambda *x: True)
-    @mock.patch('semantic_release.history.get_commit_log', lambda: [NO_TAG])
+    @mock.patch('semantic_release.history.logs.get_commit_log', lambda: [NO_TAG])
     def test_should_patch_without_tagged_commits(self):
         self.assertEqual(evaluate_version_bump('1.1.0'), 'patch')
 
     @mock.patch('semantic_release.history.config.getboolean', lambda *x: False)
-    @mock.patch('semantic_release.history.get_commit_log', lambda: [NO_TAG])
+    @mock.patch('semantic_release.history.logs.get_commit_log', lambda: [NO_TAG])
     def test_should_return_none_without_tagged_commits(self):
         self.assertIsNone(evaluate_version_bump('1.1.0'))
 
-    @mock.patch('semantic_release.history.get_commit_log', lambda: [])
+    @mock.patch('semantic_release.history.logs.get_commit_log', lambda: [])
     def test_should_return_none_without_commits(self):
         """
         Make sure that we do not release if there are no commits since last release.
