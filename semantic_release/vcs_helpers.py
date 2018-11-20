8f21700b62a752da644d019afe2d84706208f8e7
Casey Vockrodt <cmvock@hotmail.com>
2018-11-19 22:53:42 -0700

    feat: Add support for commit_message config variable
    This variable can allow you to skip CI pipelines in CI tools like GitLab
    CI by adding [CI skip] in the body. There are likely many uses for this
    beyond that particular example...
    
    BREAKING CHANGE: If you rely on the commit message to be the version
    number only, this will break your code
    
    re #88 #32

--- semantic_release/vcs_helpers.py
+++ semantic_release/vcs_helpers.py
@@ -80,8 +80,10 @@ def commit_new_version(version):
     :param version: The version number to be used in the commit message
     """
 
+    commit_message = config.get('semantic_release', 'commit_message')
+    message = '{0}\n\n{1}'.format(version, commit_message)
     repo.git.add(config.get('semantic_release', 'version_variable').split(':')[0])
-    return repo.git.commit(m=version, author="semantic-release <semantic-release>")
+    return repo.git.commit(m=message, author="semantic-release <semantic-release>")
 
 
 def tag_new_version(version):
