26e56402e8092432d3883c8f2b4b1e71b6757dec
Alan D. Tse <alandtse@gmail.com>
2019-09-28 23:51:30 -0700

    fix(github): add check for GITHUB_ACTOR for git push

--- semantic_release/vcs_helpers.py
+++ semantic_release/vcs_helpers.py
@@ -163,12 +163,22 @@ def push_new_version(
         token = auth_token
         if config.get('semantic_release', 'hvcs') == 'gitlab':
             token = 'gitlab-ci-token:' + token
-        server = 'https://{token}@{server_url}/{owner}/{name}.git'.format(
+        actor = os.environ.get('GITHUB_ACTOR')
+        if actor:
+            server = 'https://{actor}:{token}@{server_url}/{owner}/{name}.git'.format(
             token=token,
             server_url=domain,
             owner=owner,
             name=name,
+            actor=actor
         )
+        else:
+            server = 'https://{token}@{server_url}/{owner}/{name}.git'.format(
+                token=token,
+                server_url=domain,
+                owner=owner,
+                name=name,
+            )
 
     try:
         repo.git.push(server, branch)
