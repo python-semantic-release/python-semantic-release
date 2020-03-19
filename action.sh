5e3e4e552a22bfde8adbc862fd2258d6892714ea
Daniel Thwaites <danthwaites30@btinternet.com>
2020-03-19 18:28:58 +0000

    feat(actions): create GitHub Action

---
+++ action.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+
+# Copy inputs into correctly-named environment variables
+export GH_TOKEN="${INPUT_GITHUB_TOKEN}"
+export PYPI_USERNAME="${INPUT_PYPI_USERNAME}"
+export PYPI_PASSWORD="${INPUT_PYPI_PASSWORD}"
+
+# Change to configured directory
+cd "${INPUT_DIRECTORY}"
+
+# Set Git details
+git config --global user.name "github-actions"
+git config --global user.email "action@github.com"
+
+# Run Semantic Release
+DEBUG="*" semantic-release publish -D commit_author="github-actions <action@github.com>"
