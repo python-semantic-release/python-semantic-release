740f4bdb26569362acfc80f7e862fc2c750a46dd
Alan Tse <alandtse@users.noreply.github.com>
2020-03-22 07:29:30 -0700

    feat(build): allow config setting for build command (#195)
    * feat(build): allow config setting for build command
    
    BREAKING CHANGE: Previously the build_commands configuration variable set the types of bundles sent to `python setup.py`. It has been replaced by the configuration variable `build_command` which takes the full command e.g. `python setup.py sdist` or `poetry build`.
    
    Closes #188

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -13,4 +13,4 @@ def test_build_command(mocker, commands):
     mocker.patch('semantic_release.dist.config.get', lambda *a: commands)
     mock_run = mocker.patch('semantic_release.dist.run')
     build_dists()
-    mock_run.assert_called_once_with('python setup.py ' + commands)
+    mock_run.assert_called_once_with(commands)
