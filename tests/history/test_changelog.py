14e4ae2a527b29026f49ee1346cab708114e60c9
Daniel Thwaites <danthwaites30@btinternet.com>
2020-05-03 23:10:30 +0100

    test: split history tests into multiple files

---
+++ tests/history/test_changelog.py
@@ -0,0 +1,94 @@
+import mock
+import pytest
+
+from semantic_release.history.logs import generate_changelog
+
+from . import *
+
+
+def test_should_generate_all_sections():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **k: ALL_KINDS_OF_COMMIT_MESSAGES + [MAJOR2, UNKNOWN_STYLE],
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert "feature" in changelog
+        assert len(changelog["feature"]) > 0
+        assert "fix" in changelog
+        assert len(changelog["fix"]) > 0
+        assert "documentation" in changelog
+        assert "breaking" in changelog
+        assert len(changelog["breaking"]) > 0
+
+def test_should_include_hash_in_section_contents():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **k: ALL_KINDS_OF_COMMIT_MESSAGES,
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert changelog["breaking"][0][0] == MAJOR[0]
+        assert changelog["feature"][0][0] == MINOR[0]
+        assert changelog["fix"][0][0] == PATCH[0]
+
+def test_should_only_read_until_given_version():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **k: MAJOR_LAST_RELEASE_MINOR_AFTER,
+    ):
+        changelog = generate_changelog("1.1.0")
+        assert len(changelog["feature"]) > 0
+        assert len(changelog["fix"]) == 0
+        assert len(changelog["documentation"]) == 0
+        assert len(changelog["breaking"]) == 0
+
+def test_should_skip_style_changes():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **k: PATCH_COMMIT_MESSAGES + [("21", "style(x): change x")],
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert "style" not in changelog
+
+def test_should_skip_chore_changes():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: PATCH_COMMIT_MESSAGES + [("23", "chore(x): change x")],
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert "chore" not in changelog
+
+@pytest.mark.parametrize("commit,expected_description", [
+    (MAJOR, "Uses super-feature as default instead of dull-feature."),
+    (MAJOR2, "Uses super-feature as default instead of dull-feature."),
+    (
+        MAJOR_MENTIONING_1_0_0,
+        "Uses super-feature as default instead of dull-feature from v1.0.0.",
+    ),
+    (MAJOR_EXCL_WITH_FOOTER, "Another feature, another breaking change"),
+    (MAJOR_EXCL_NOT_FOOTER, "Fix a big bug that everyone exploited"),
+])
+def test_should_get_right_breaking_description(commit, expected_description):
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: [commit],
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert changelog["breaking"][0][1] == expected_description
+
+def test_should_get_multiple_breaking_descriptions():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: [MAJOR_MULTIPLE_FOOTERS],
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert len(changelog["breaking"]) == 2
+        assert changelog["breaking"][0][1] == "Breaking change 1"
+        assert changelog["breaking"][1][1] == "Breaking change 2"
+
+def test_messages_are_capitalized():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **k: [("23", "fix(x): abCD")],
+    ):
+        changelog = generate_changelog("0.0.0")
+        assert changelog["fix"][0][1] == "AbCD"
---
+++ tests/history/test_version.py
@@ -0,0 +1,80 @@
+import mock
+
+import semantic_release
+from semantic_release.history import (
+    get_current_version,
+    get_current_version_by_tag,
+    get_new_version,
+    get_previous_version,
+    set_new_version,
+)
+
+from ..mocks import mock_version_file
+
+
+def test_current_version_should_return_correct_version():
+    assert get_current_version() == semantic_release.__version__
+
+
+@mock.patch("semantic_release.history.get_last_version", return_value="last_version")
+def test_current_version_should_return_git_version(mock_last_version):
+    assert "last_version" == get_current_version_by_tag()
+
+
+@mock.patch("semantic_release.history.config.get", return_value="tag")
+@mock.patch("semantic_release.history.get_last_version", return_value=None)
+def test_current_version_should_return_default_version(mock_config, mock_last_version):
+    assert "0.0.0" == get_current_version()
+
+
+class TestGetPreviousVersion():
+    @mock.patch(
+        "semantic_release.history.get_commit_log",
+        lambda: [("211", "0.10.0"), ("13", "0.9.0")],
+    )
+    def test_should_return_correct_version(self):
+        assert get_previous_version("0.10.0") == "0.9.0"
+
+    @mock.patch(
+        "semantic_release.history.get_commit_log",
+        lambda: [("211", "0.10.0"), ("13", "0.9.0")],
+    )
+    def test_should_return_correct_version_with_v(self):
+        assert get_previous_version("0.10.0") == "0.9.0"
+
+
+class TestGetNewVersion():
+    def test_major_bump(self):
+        assert get_new_version("0.0.0", "major") == "1.0.0"
+        assert get_new_version("0.1.0", "major") == "1.0.0"
+        assert get_new_version("0.1.9", "major") == "1.0.0"
+        assert get_new_version("10.1.0", "major") == "11.0.0"
+
+    def test_minor_bump(self):
+        assert get_new_version("0.0.0", "minor") == "0.1.0"
+        assert get_new_version("1.2.0", "minor") == "1.3.0"
+        assert get_new_version("1.2.1", "minor") == "1.3.0"
+        assert get_new_version("10.1.0", "minor") == "10.2.0"
+
+    def test_patch_bump(self):
+        assert get_new_version("0.0.0", "patch") == "0.0.1"
+        assert get_new_version("0.1.0", "patch") == "0.1.1"
+        assert get_new_version("10.0.9", "patch") == "10.0.10"
+
+    def test_none_bump(self):
+        assert get_new_version("1.0.0", None) == "1.0.0"
+
+
+@mock.patch("builtins.open", mock_version_file)
+@mock.patch(
+    "semantic_release.history.config.get", return_value="my_version_path:my_version_var"
+)
+def test_set_version(mock_config):
+    set_new_version("X.Y.Z")
+
+    handle_open = mock_version_file()
+    mock_version_file.assert_any_call("my_version_path", mode="w")
+    mock_version_file.assert_any_call("my_version_path", mode="r")
+    handle_open.read.assert_called_once_with()
+    handle_open.write.assert_called_once_with("my_version_var = 'X.Y.Z'")
+    mock_version_file.reset_mock()
