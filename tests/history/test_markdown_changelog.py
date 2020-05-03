feed80217e21dbb7a02964092fddc36cca5a1f73
Daniel Thwaites <danthwaites30@btinternet.com>
2020-05-03 23:10:30 +0100

    test: split history tests into multiple files

---
+++ tests/history/test_markdown_changelog.py
@@ -0,0 +1,74 @@
+from semantic_release.history.logs import markdown_changelog
+
+
+def test_should_output_all_sections():
+    assert markdown_changelog(
+        "0",
+        {
+            "refactor": [("12", "Refactor super-feature")],
+            "breaking": [
+                ("21", "Uses super-feature as default instead of dull-feature.")
+            ],
+            "feature": [
+                ("145", "Add non-breaking super-feature"),
+                ("134", "Add super-feature"),
+            ],
+            "fix": [("234", "Fix bug in super-feature")],
+            "documentation": [("0", "Document super-feature")],
+            "performance": [],
+        },
+    ) == (
+        "\n"
+        "### Feature\n"
+        "* Add non-breaking super-feature (145)\n"
+        "* Add super-feature (134)\n"
+        "\n"
+        "### Fix\n"
+        "* Fix bug in super-feature (234)\n"
+        "\n"
+        "### Breaking\n"
+        "* Uses super-feature as default instead of dull-feature. (21)\n"
+        "\n"
+        "### Documentation\n"
+        "* Document super-feature (0)\n"
+    )
+
+def test_should_not_include_empty_sections():
+    assert markdown_changelog(
+        "1.0.1",
+        {
+            "refactor": [],
+            "breaking": [],
+            "feature": [],
+            "fix": [],
+            "documentation": [],
+            "performance": [],
+        },
+    ) == ""
+
+def test_should_not_output_heading():
+    assert "v1.0.1" not in markdown_changelog(
+        "1.0.1",
+        {
+            "refactor": [],
+            "breaking": [],
+            "feature": [],
+            "fix": [],
+            "documentation": [],
+            "performance": [],
+        },
+    )
+
+def test_should_output_heading():
+    assert "## v1.0.1\n" in markdown_changelog(
+        "1.0.1",
+        {
+            "refactor": [],
+            "breaking": [],
+            "feature": [],
+            "fix": [],
+            "documentation": [],
+            "performance": [],
+        },
+        header=True,
+    )
---
+++ tests/history/test_version_bump.py
@@ -0,0 +1,88 @@
+import mock
+
+from semantic_release.history import evaluate_version_bump, replace_version_string
+
+from . import *
+
+
+def test_major():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: ALL_KINDS_OF_COMMIT_MESSAGES,
+    ):
+        assert evaluate_version_bump("0.0.0") == "major"
+
+def test_minor():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: MINOR_AND_PATCH_COMMIT_MESSAGES,
+    ):
+        assert evaluate_version_bump("0.0.0") == "minor"
+
+def test_patch():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: PATCH_COMMIT_MESSAGES,
+    ):
+        assert evaluate_version_bump("0.0.0") == "patch"
+
+def test_nothing_if_no_tag():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: [("", "...")],
+    ):
+        assert evaluate_version_bump("0.0.0") is None
+
+def test_force():
+    assert evaluate_version_bump("0.0.0", "major") == "major"
+    assert evaluate_version_bump("0.0.0", "minor") == "minor"
+    assert evaluate_version_bump("0.0.0", "patch") == "patch"
+
+def test_should_not_skip_commits_mentioning_other_commits():
+    with mock.patch(
+        "semantic_release.history.logs.get_commit_log",
+        lambda *a, **kw: MAJOR_MENTIONING_LAST_VERSION,
+    ):
+        assert evaluate_version_bump("1.0.0") == "major"
+
+@mock.patch(
+    "semantic_release.history.config.getboolean", lambda *x: x == PATCH_WIHTOUT_TAG
+)
+@mock.patch(
+    "semantic_release.history.logs.get_commit_log", lambda *a, **kw: [MINOR]
+)
+def test_should_minor_with_patch_without_tag():
+    assert evaluate_version_bump("1.1.0") == "minor"
+
+@mock.patch(
+    "semantic_release.history.config.getboolean", lambda *x: x == PATCH_WIHTOUT_TAG
+)
+@mock.patch(
+    "semantic_release.history.logs.get_commit_log", lambda *a, **kw: [NO_TAG]
+)
+def test_should_patch_without_tagged_commits():
+    assert evaluate_version_bump("1.1.0") == "patch"
+
+@mock.patch(
+    "semantic_release.history.config.getboolean", lambda *x: x != PATCH_WIHTOUT_TAG
+)
+@mock.patch(
+    "semantic_release.history.logs.get_commit_log", lambda *a, **kw: [NO_TAG]
+)
+def test_should_return_none_without_tagged_commits():
+    assert evaluate_version_bump("1.1.0") is None
+
+@mock.patch("semantic_release.history.logs.get_commit_log", lambda *a, **kw: [])
+def test_should_return_none_without_commits():
+    """
+    Make sure that we do not release if there are no commits since last release.
+    """
+    with mock.patch("semantic_release.history.config.getboolean", lambda *x: True):
+        assert evaluate_version_bump("1.1.0") is None
+
+    with mock.patch("semantic_release.history.config.getboolean", lambda *x: False):
+        assert evaluate_version_bump("1.1.0") is None
+
+def test_version_bump_maintains_formatting():
+    assert replace_version_string('ver="1.2.3"', "ver", "1.2.4") == 'ver="1.2.4"'
+    assert replace_version_string("version = '1.2.3'", "version", "1.2.4") == "version = '1.2.4'"
