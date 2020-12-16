f3ece78b2913e70f6b99907b192a1e92bbfd6b77
Toilal <toilal.dev@gmail.com>
2020-12-16 18:53:35 +0100

    fix: filenames with unknown mimetype are now properly uploaded to github release
    When mimetype can't be guessed, content-type header is set to None.
    But it's mandatory for the file upload to work properly.
    In this case, application/octect-stream is now used as a fallback.

--- tests/test_hvcs.py
+++ tests/test_hvcs.py
@@ -151,6 +151,10 @@ class GithubReleaseTests(TestCase):
         "https://uploads.github.com/repos/relekang/rmoq/releases/1/assets"
         "?name=testupload.md&label=Dummy+file"
     )
+    asset_no_extension_url_params = (
+        "https://uploads.github.com/repos/relekang/rmoq/releases/1/assets"
+        "?name=testupload-no-extension&label=Dummy+file+no+extension"
+    )
     dist_asset_url_params = (
         "https://uploads.github.com/repos/relekang/rmoq/releases/1/assets"
         "?name=testupload.md"
@@ -278,6 +282,36 @@ class GithubReleaseTests(TestCase):
         )
         status = Github.upload_asset(
             "relekang", "rmoq", 1, dummy_file_path, "Dummy file"
+        )
+        self.assertTrue(status)
+
+        # Remove test file
+        os.remove(dummy_file_path)
+
+
+    @responses.activate
+    @mock.patch("semantic_release.hvcs.Github.token", return_value="super-token")
+    def test_should_upload_asset_with_no_extension(self, mock_token):
+        # Create temporary file to upload
+        dummy_file_path = os.path.join(temp_dir, "testupload-no-extension")
+        os.makedirs(os.path.dirname(dummy_file_path), exist_ok=True)
+        dummy_content = "# Test File with no extension\n\n*Dummy asset for testing uploads.*"
+        with open(dummy_file_path, "w") as dummy_file:
+            dummy_file.write(dummy_content)
+
+        def request_callback(request):
+            self.assertEqual(request.body.decode().replace("\r\n", "\n"), dummy_content)
+            self.assertEqual(request.url, self.asset_no_extension_url_params)
+            self.assertEqual(request.headers["Content-Type"], "application/octet-stream")
+            self.assertEqual("token super-token", request.headers["Authorization"])
+
+            return 201, {}, json.dumps({})
+
+        responses.add_callback(
+            responses.POST, self.asset_url, callback=request_callback
+        )
+        status = Github.upload_asset(
+            "relekang", "rmoq", 1, dummy_file_path, "Dummy file no extension"
         )
         self.assertTrue(status)
 
