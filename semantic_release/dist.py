527f02bded40c46bc61f0aad57b707762ef0fba5
Toilal <toilal.dev@gmail.com>
2021-01-07 21:03:00 +0100

    test(build): add tests for should_build() and should_remove_dist()

--- semantic_release/dist.py
+++ semantic_release/dist.py
@@ -13,12 +13,12 @@ def should_build():
     upload_pypi = config.get("upload_to_pypi")
     upload_release = config.get("upload_to_release")
     build_command = config.get("build_command")
-    return build_command and (upload_pypi or upload_release)
+    return bool(build_command and (upload_pypi or upload_release))
 
 
 def should_remove_dist():
     remove_dist = config.get("remove_dist")
-    return remove_dist and should_build()
+    return bool(remove_dist and should_build())
 
 
 def build_dists():
