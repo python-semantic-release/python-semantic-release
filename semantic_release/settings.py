c2ecb50742709c83283e3bb1208aa5701b980954
Bruno Alla <browniebroke@users.noreply.github.com>
2021-01-28 18:42:12 +0000

    fix: crash when TOML has no PSR section

--- semantic_release/settings.py
+++ semantic_release/settings.py
@@ -67,7 +67,7 @@ def _config_from_pyproject(path):
             with open(path, "r") as f:
                 pyproject = tomlkit.loads(f.read())
             if pyproject:
-                return dict(pyproject.get("tool").get("semantic_release"))
+                return pyproject.get("tool", {}).get("semantic_release", {})
         except TOMLKitError as e:
             logger.debug(f"Could not decode pyproject.toml: {e}")
 
