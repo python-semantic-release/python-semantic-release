f4e69811e06bfaf19d4ae4fa69e7dc2c83deee38
Bernard Cooke <bernard-cooke@hotmail.com>
2022-09-05 10:00:53 +0100

    psr8-27 | refactor angular commit parser tests

--- tests/unit/semantic_release/commit_parser/test_angular.py
+++ tests/unit/semantic_release/commit_parser/test_angular.py
@@ -27,167 +27,99 @@ def test_parser_raises_unknown_message_style(default_angular_parser):
         )
 
 
-def test_parser_returns_correct_bump_level(default_angular_parser):
-    assert (
-        default_angular_parser.parse(
-            make_commit("feat(parsers): Add new parser pattern\n\nBREAKING CHANGE: ")
-        ).bump
-        is LevelBump.MAJOR
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit("feat(parsers)!: Add new parser pattern")
-        ).bump
-        is LevelBump.MAJOR
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit(
-                "feat(parsers): Add new parser pattern\n\n"
-                "New pattern is awesome\n\nBREAKING CHANGE: \n"
-            )
-        ).bump
-        is LevelBump.MAJOR
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit(
-                "feat(parsers): Add new parser pattern\n\nBREAKING-CHANGE: change !"
-            )
-        ).bump
-        is LevelBump.MAJOR
-    )
-    assert (
-        default_angular_parser.parse(make_commit("feat(parser): Add emoji parser")).bump
-        is LevelBump.MINOR
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit("fix(parser): Fix regex in angular parser")
-        ).bump
-        is LevelBump.PATCH
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit("test(parser): Add a test for angular parser")
-        ).bump
-        is LevelBump.NO_RELEASE
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit("feat(parser)!: Edit dat parsing stuff")
-        ).bump
-        is LevelBump.MAJOR
-    )
-    assert (
-        default_angular_parser.parse(make_commit("fix!: Edit dat parsing stuff again")).bump
-        is LevelBump.MAJOR
-    )
+@pytest.mark.parametrize(
+    "commit_message, bump",
+    [
+        ("feat(parsers): Add new parser pattern\n\nBREAKING CHANGE: ", LevelBump.MAJOR),
+        ("feat(parsers)!: Add new parser pattern", LevelBump.MAJOR),
+        (
+            "feat(parsers): Add new parser pattern\n\nNew pattern is awesome\n\nBREAKING CHANGE: \n",
+            LevelBump.MAJOR,
+        ),
+        (
+            "feat(parsers): Add new parser pattern\n\nBREAKING-CHANGE: change !",
+            LevelBump.MAJOR,
+        ),
+        ("feat(parser): Add emoji parser", LevelBump.MINOR),
+        ("fix(parser): Fix regex in angular parser", LevelBump.PATCH),
+        ("test(parser): Add a test for angular parser", LevelBump.NO_RELEASE),
+        ("feat(parser)!: Edit dat parsing stuff", LevelBump.MAJOR),
+        ("fix!: Edit dat parsing stuff again", LevelBump.MAJOR),
+        ("fix: superfix", LevelBump.PATCH),
+    ],
+)
+def test_parser_returns_correct_bump_level(
+    default_angular_parser, commit_message, bump
+):
+    assert default_angular_parser.parse(make_commit(commit_message)).bump is bump
 
 
-def test_parser_return_type_from_commit_message(default_angular_parser):
-    assert (
-        default_angular_parser.parse(make_commit("feat(parser): ...")).type == "feature"
-    )
-    assert default_angular_parser.parse(make_commit("fix(parser): ...")).type == "fix"
-    assert default_angular_parser.parse(make_commit("test(parser): ...")).type == "test"
-    assert (
-        default_angular_parser.parse(make_commit("docs(parser): ...")).type
-        == "documentation"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("style(parser): ...")).type == "style"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("refactor(parser): ...")).type
-        == "refactor"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(parser): ...")).type == "chore"
-    )
+@pytest.mark.parametrize(
+    "message, type_",
+    [
+        ("feat(parser): ...", "feature"),
+        ("fix(parser): ...", "fix"),
+        ("test(parser): ...", "test"),
+        ("docs(parser): ...", "documentation"),
+        ("style(parser): ...", "style"),
+        ("refactor(parser): ...", "refactor"),
+        ("chore(parser): ...", "chore"),
+    ],
+)
+def test_parser_return_type_from_commit_message(default_angular_parser, message, type_):
+    assert default_angular_parser.parse(make_commit(message)).type == type_
 
 
-def test_parser_return_scope_from_commit_message(default_angular_parser):
-    assert (
-        default_angular_parser.parse(make_commit("chore(parser): ...")).scope
-        == "parser"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(a part): ...")).scope
-        == "a part"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(a_part): ...")).scope
-        == "a_part"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(a-part): ...")).scope
-        == "a-part"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(a.part): ...")).scope
-        == "a.part"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(a+part): ...")).scope
-        == "a+part"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore(a&part): ...")).scope
-        == "a&part"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore((part)): ...")).scope
-        == "(part)"
-    )
-    assert (
-        default_angular_parser.parse(make_commit("chore((p):rt): ...")).scope
-        == "(p):rt"
-    )
+@pytest.mark.parametrize(
+    "message, scope",
+    [
+        ("chore(parser): ...", "parser"),
+        ("chore(a part): ...", "a part"),
+        ("chore(a_part): ...", "a_part"),
+        ("chore(a-part): ...", "a-part"),
+        ("chore(a.part): ...", "a.part"),
+        ("chore(a+part): ...", "a+part"),
+        ("chore(a&part): ...", "a&part"),
+        ("chore((part)): ...", "(part)"),
+        ("chore((p):rt): ...", "(p):rt"),
+    ],
+)
+def test_parser_return_scope_from_commit_message(
+    default_angular_parser, message, scope
+):
+    assert default_angular_parser.parse(make_commit(message)).scope == scope
 
 
-def test_parser_return_subject_from_commit_message(default_angular_parser):
-    assert (
-        default_angular_parser.parse(
-            make_commit("feat(parser): Add emoji parser")
-        ).descriptions[0]
-        == "Add emoji parser"
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit("fix(parser): Fix regex in angular parser")
-        ).descriptions[0]
-        == "Fix regex in angular parser"
-    )
-    assert (
-        default_angular_parser.parse(
-            make_commit("test(parser): Add a test for angular parser")
-        ).descriptions[0]
-        == "Add a test for angular parser"
-    )
+_long_text = (
+    "This is an long explanatory part of a commit message. It should give "
+    "some insight to the fix this commit adds to the codebase."
+)
+_footer = "Closes #400"
 
 
-def test_parser_return_footer_from_commit_message(default_angular_parser):
-    text = (
-        "This is an long explanatory part of a commit message. It should give "
-        "some insight to the fix this commit adds to the codebase."
-    )
-    footer = "Closes #400"
-    commit = make_commit(
-        "fix(tox): Fix env \n\n{text}\n\n{footer}".format_map(locals())
-    )
-    assert default_angular_parser.parse(commit).descriptions[1] == text
-    assert default_angular_parser.parse(commit).descriptions[2] == footer
-
-
-def test_parser_should_accept_message_without_scope(default_angular_parser):
+@pytest.mark.parametrize(
+    "message, descriptions",
+    [
+        ("feat(parser): Add emoji parser", ["Add emoji parser"]),
+        ("fix(parser): Fix regex in angular parser", ["Fix regex in angular parser"]),
+        (
+            "test(parser): Add a test for angular parser",
+            ["Add a test for angular parser"],
+        ),
+        (
+            "fix(tox): Fix env \n\n{text}\n\n{footer}".format(
+                text=_long_text, footer=_footer
+            ),
+            ["Fix env ", _long_text, _footer],
+        ),
+        ("fix: superfix", ["superfix"]),
+    ],
+)
+def test_parser_return_subject_from_commit_message(
+    default_angular_parser, message, descriptions
+):
     assert (
-        default_angular_parser.parse(make_commit("fix: superfix")).bump
-        is LevelBump.PATCH
-    )
-    assert (
-        default_angular_parser.parse(make_commit("fix: superfix")).descriptions[0]
-        == "superfix"
+        default_angular_parser.parse(make_commit(message)).descriptions == descriptions
     )
 
 
