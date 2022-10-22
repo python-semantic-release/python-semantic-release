c8700ab22a2ecdafd905cf7dd8dbe38f17c34b52
Bernard Cooke <bernard-cooke@hotmail.com>
2022-10-22 18:28:00 +0100

    delete tests.old

--- semantic_release/cli/__init__.py
+++ semantic_release/cli/__init__.py
@@ -1,5 +1,3 @@
-# import sys
-
 from semantic_release.cli.commands.changelog import changelog
 from semantic_release.cli.commands.generate_config import generate_config
 from semantic_release.cli.commands.main import main
@@ -10,10 +8,3 @@ main.add_command(changelog)
 main.add_command(generate_config)
 main.add_command(version)
 main.add_command(publish)
-
-"""
-def entry() -> None:
-    # Move flags to after the command
-    # args = sorted(sys.argv[1:], key=lambda x: 1 if x.startswith("--") else -1)
-    main(args=sys.argv[1:])
-"""
