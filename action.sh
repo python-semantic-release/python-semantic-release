4664afe5f80a04834e398fefb841b166a51d95b7
Cooper Mitzel <84025850+cmitzel-ncino@users.noreply.github.com>
2022-11-01 14:30:04 -0400

    feat: Update action with configuration options (#518)
    Co-authored-by: Kevin Watson <Kevmo92@users.noreply.github.com>

--- action.sh
+++ action.sh
@@ -8,17 +8,19 @@ export PYPI_TOKEN="${INPUT_PYPI_TOKEN}"
 export REPOSITORY_USERNAME="${INPUT_REPOSITORY_USERNAME}"
 export REPOSITORY_PASSWORD="${INPUT_REPOSITORY_PASSWORD}"
 export PATH="${PATH}:/semantic-release/.venv/bin"
+export GIT_COMMITER_NAME="${INPUT_GIT_COMMITER_NAME:="github-actions"}"
+export GIT_COMMITER_EMAIL="${INPUT_GIT_COMMITER_EMAIL:="github-actions@github.com"}"
 
 # Change to configured directory
 cd "${INPUT_DIRECTORY}"
 
 # Set Git details
-git config --global user.name "github-actions"
-git config --global user.email "github-actions@github.com"
+git config --global user.name "$GIT_COMMITER_NAME"
+git config --global user.email "$GIT_COMMITER_EMAIL"
 
 # Run Semantic Release
 /semantic-release/.venv/bin/python \
   -m semantic_release publish \
   -v DEBUG \
-  -D commit_author="github-actions <action@github.com>" \
+  -D commit_author="$GIT_COMMITER_NAME <$GIT_COMMITER_EMAIL>" \
   ${INPUT_ADDITIONAL_OPTIONS}
