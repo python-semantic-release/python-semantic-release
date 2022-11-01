1d2d9c53e5384e841f980936fe1339fdf77f557b
Cooper <cooper.mitzel@ncino.com>
2022-11-01 14:22:42 -0400

    clean up the envars

--- action.sh
+++ action.sh
@@ -8,12 +8,11 @@ export PYPI_TOKEN="${INPUT_PYPI_TOKEN}"
 export REPOSITORY_USERNAME="${INPUT_REPOSITORY_USERNAME}"
 export REPOSITORY_PASSWORD="${INPUT_REPOSITORY_PASSWORD}"
 export PATH="${PATH}:/semantic-release/.venv/bin"
+export GIT_COMMITER_NAME="${INPUT_GIT_COMMITER_NAME:="github-actions"}"
+export GIT_COMMITER_EMAIL="${INPUT_GIT_COMMITER_EMAIL:="github-actions@github.com"}"
 
 # Change to configured directory
 cd "${INPUT_DIRECTORY}"
-
-export GIT_COMMITER_NAME="${INPUT_GIT_COMMITER_NAME:="github-actions"}"
-export GIT_COMMITER_EMAIL="${INPUT_GIT_COMMITER_EMAIL:="github-actions@github.com"}"
 
 # Set Git details
 git config --global user.name "$GIT_COMMITER_NAME"
