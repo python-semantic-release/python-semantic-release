45824cf63f199ceffcc1617c3cfe003a4d0aa474
Bernard Cooke <bernard-cooke@hotmail.com>
2023-02-01 09:08:26 +0000

    fix(action): quotation for git config command

--- action.sh
+++ action.sh
@@ -25,7 +25,7 @@ git config --global user.email "$GIT_COMMITTER_EMAIL"
 
 # See https://github.com/actions/runner-images/issues/6775#issuecomment-1409268124
 # and https://github.com/actions/runner-images/issues/6775#issuecomment-1410270956
-git config --system --add safe.directory *
+git config --system --add safe.directory "*"
 
 if [[  -n $SSH_PUBLIC_SIGNING_KEY && -n $SSH_PRIVATE_SIGNING_KEY ]]; then
     echo "SSH Key pair found, configuring signing..."
