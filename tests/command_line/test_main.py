65d4e367f57614c8ff43e1ab41e31fc7c1776b46
Bernard Cooke <bernard-cooke@hotmail.com>
2023-07-18 22:08:07 +0100

    fix: handle missing configuration
    With this change, PSR will no longer error if it cannot
    find appropriate configuration in the default
    pyproject.toml file, or in the file specified by -c/
    --config when that option is given. Instead it will
    use the default configuration values

--- tests/command_line/test_main.py
+++ tests/command_line/test_main.py
@@ -1,3 +1,8 @@
+import json
+import os
+from textwrap import dedent
+
+import git
 import pytest
 
 from semantic_release import __version__
@@ -30,3 +35,92 @@ def test_not_a_release_branch_exit_code_with_strict(
     repo_with_git_flow_angular_commits.git.checkout("-b", "branch-does-not-exist")
     result = cli_runner.invoke(main, ["--strict", "version", "--no-commit"])
     assert result.exit_code != 0
+
+
+@pytest.fixture
+def toml_file_with_no_configuration_for_psr(tmp_path):
+    path = tmp_path / "config.toml"
+    path.write_text(
+        dedent(
+            r"""
+            [project]
+            name = "foo"
+            version = "1.2.0"
+            """
+        )
+    )
+
+    yield path
+
+
+@pytest.fixture
+def json_file_with_no_configuration_for_psr(tmp_path):
+    path = tmp_path / "config.json"
+    path.write_text(json.dumps({"foo": [1, 2, 3]}))
+
+    yield path
+
+
+@pytest.mark.usefixtures("repo_with_git_flow_angular_commits")
+def test_default_config_is_used_when_none_in_toml_config_file(
+    cli_runner,
+    toml_file_with_no_configuration_for_psr,
+):
+    result = cli_runner.invoke(
+        main,
+        ["--noop", "--config", str(toml_file_with_no_configuration_for_psr), "version"],
+    )
+
+    assert result.exit_code == 0
+
+
+@pytest.mark.usefixtures("repo_with_git_flow_angular_commits")
+def test_default_config_is_used_when_none_in_json_config_file(
+    cli_runner,
+    json_file_with_no_configuration_for_psr,
+):
+    result = cli_runner.invoke(
+        main,
+        ["--noop", "--config", str(json_file_with_no_configuration_for_psr), "version"],
+    )
+
+    assert result.exit_code == 0
+
+
+@pytest.mark.usefixtures("repo_with_git_flow_angular_commits")
+def test_errors_when_config_file_does_not_exist_and_passed_explicitly(
+    cli_runner,
+):
+    result = cli_runner.invoke(
+        main,
+        ["--noop", "--config", "somenonexistantfile.123.txt", "version"],
+    )
+
+    assert result.exit_code == 2
+    assert "does not exist" in result.stderr
+
+
+def test_uses_default_config_when_no_config_file_found(
+    tmp_path,
+    cli_runner,
+):
+    # We have to initialise an empty git repository, as the example projects
+    # all have pyproject.toml configs which would be used by default
+    repo = git.Repo.init(tmp_path)
+    repo.git.branch("-M", "main")
+    with repo.config_writer("repository") as config:
+        config.set_value("user", "name", "semantic release testing")
+        config.set_value("user", "email", "not_a_real@email.com")
+    repo.create_remote(name="origin", url="foo@barvcs.com:user/repo.git")
+    repo.git.commit("-m", "feat: initial commit", "--allow-empty")
+
+    try:
+        os.chdir(tmp_path)
+        result = cli_runner.invoke(
+            main,
+            ["--noop", "version"],
+        )
+
+        assert result.exit_code == 0
+    finally:
+        repo.close()
