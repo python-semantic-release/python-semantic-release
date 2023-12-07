744ff256f10c895a97241dc085bc132d10c9f737
codejedi365 <codejedi365@users.noreply.github.com>
2023-12-07 11:25:14 -0500

    test(commandline-main): prevent git gpgsign config from failing tests (#760)

--- tests/command_line/test_main.py
+++ tests/command_line/test_main.py
@@ -111,6 +111,7 @@ def test_uses_default_config_when_no_config_file_found(
     with repo.config_writer("repository") as config:
         config.set_value("user", "name", "semantic release testing")
         config.set_value("user", "email", "not_a_real@email.com")
+        config.set_value("commit", "gpgsign", False)
     repo.create_remote(name="origin", url="foo@barvcs.com:user/repo.git")
     repo.git.commit("-m", "feat: initial commit", "--allow-empty")
 
