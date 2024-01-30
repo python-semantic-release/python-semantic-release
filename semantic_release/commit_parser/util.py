70193ba117c1a6d3690aed685fee8a734ba174e5
codejedi365 <codejedi365@gmail.com>
2024-01-29 23:54:56 -0500

    fix(util): properly parse windows line-endings in commit messages
    Due to windows line-endings `\r\n`, it would improperly split the commit
    description (it failed to split at all) and cause detection of Breaking changes
    to fail. The breaking changes regular expression looks to the start of the line
    for the proper syntax.
    
    Resolves: #820

--- semantic_release/commit_parser/util.py
+++ semantic_release/commit_parser/util.py
@@ -9,11 +9,14 @@ def parse_paragraphs(text: str) -> list[str]:
     """
     This will take a text block and return a list containing each
     paragraph with single line breaks collapsed into spaces.
+
+    To handle Windows line endings, carriage returns '\r' are removed before
+    separating into paragraphs.
+
     :param text: The text string to be divided.
     :return: A list of condensed paragraphs, as strings.
     """
-    return [
-        paragraph.replace("\n", " ")
-        for paragraph in text.split("\n\n")
-        if len(paragraph) > 0
-    ]
+    return list(filter(None, [
+        paragraph.replace("\n", " ").strip()
+        for paragraph in text.replace("\r", "").split("\n\n")
+    ]))
