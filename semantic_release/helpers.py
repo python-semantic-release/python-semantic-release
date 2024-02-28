27cd93a0a65ee3787ca51be4c91c48f6ddb4269c
andrew-kline <65789862+andrew-kline@users.noreply.github.com>
2024-02-27 21:39:33 -0800

    fix(parse-git-url): handle urls with url-safe special characters

--- semantic_release/helpers.py
+++ semantic_release/helpers.py
@@ -114,12 +114,12 @@ def parse_git_url(url: str) -> ParsedGitUrl:
     # Normalizers are a list of tuples of (pattern, replacement)
     normalizers = [
         # normalize implicit ssh urls to explicit ssh://
-        (r"^(\w+@)", r"ssh://\1"),
+        (r"^([\w._-]+@)", r"ssh://\1"),
         # normalize git+ssh:// urls to ssh://
         (r"^git\+ssh://", "ssh://"),
         # normalize an scp like syntax to URL compatible syntax
         # excluding port definitions (:#####) & including numeric usernames
-        (r"(ssh://(?:\w+@)?[\w.-]+):(?!\d{1,5}/\w+/)(.*)$", r"\1/\2"),
+        (r"(ssh://(?:[\w._-]+@)?[\w.-]+):(?!\d{1,5}/\w+/)(.*)$", r"\1/\2"),
         # normalize implicit file (windows || posix) urls to explicit file:// urls
         (r"^([C-Z]:/)|^/(\w)", r"file:///\1\2"),
     ]
