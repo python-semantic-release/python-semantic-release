bcf3c86954b6e7d1bd590b57ac7d37cc78da44de
codejedi365 <codejedi365@gmail.com>
2024-04-24 22:35:21 -0400

    refactor(remote-hvcs-base): extend the base abstract class with common remote base class

--- semantic_release/hvcs/__init__.py
+++ semantic_release/hvcs/__init__.py
@@ -3,6 +3,15 @@ from semantic_release.hvcs.bitbucket import Bitbucket
 from semantic_release.hvcs.gitea import Gitea
 from semantic_release.hvcs.github import Github
 from semantic_release.hvcs.gitlab import Gitlab
+from semantic_release.hvcs.remote_hvcs_base import RemoteHvcsBase
 from semantic_release.hvcs.token_auth import TokenAuth
 
-__all__ = ["Bitbucket", "Gitea", "Github", "Gitlab", "HvcsBase", "TokenAuth"]
+__all__ = [
+    "Bitbucket",
+    "Gitea",
+    "Github",
+    "Gitlab",
+    "HvcsBase",
+    "RemoteHvcsBase",
+    "TokenAuth",
+]
