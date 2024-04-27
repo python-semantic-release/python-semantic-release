83c514620ee1ee043f635770066b83508833bb9c
codejedi365 <codejedi365@gmail.com>
2024-04-27 18:22:10 -0400

    test(fixtures): redefine parser fixtures with new default option function

--- tests/fixtures/commit_parsers.py
+++ tests/fixtures/commit_parsers.py
@@ -2,38 +2,45 @@ import pytest
 
 from semantic_release.commit_parser import (
     AngularCommitParser,
+    AngularParserOptions,
     EmojiCommitParser,
+    EmojiParserOptions,
     TagCommitParser,
+    TagParserOptions,
 )
 
 # Note scipy defined in ./scipy.py as already used there
 
 
-@pytest.fixture
-def default_angular_parser_options():
-    return AngularCommitParser.parser_options()
+@pytest.fixture(scope="session")
+def default_angular_parser() -> AngularCommitParser:
+    return AngularCommitParser()
 
 
-@pytest.fixture
-def default_angular_parser(default_angular_parser_options):
-    return AngularCommitParser(options=default_angular_parser_options)
+@pytest.fixture(scope="session")
+def default_angular_parser_options(
+    default_angular_parser: AngularCommitParser,
+) -> AngularParserOptions:
+    return default_angular_parser.get_default_options()
 
 
-@pytest.fixture
-def default_emoji_parser_options():
-    return EmojiCommitParser.parser_options()
+@pytest.fixture(scope="session")
+def default_emoji_parser() -> EmojiCommitParser:
+    return EmojiCommitParser()
 
 
-@pytest.fixture
-def default_emoji_parser(default_emoji_parser_options):
-    return EmojiCommitParser(options=default_emoji_parser_options)
+@pytest.fixture(scope="session")
+def default_emoji_parser_options(
+    default_emoji_parser: EmojiCommitParser,
+) -> EmojiParserOptions:
+    return default_emoji_parser.get_default_options()
 
 
-@pytest.fixture
-def default_tag_parser_options():
-    return TagCommitParser.parser_options()
+@pytest.fixture(scope="session")
+def default_tag_parser() -> TagCommitParser:
+    return TagCommitParser()
 
 
-@pytest.fixture
-def default_tag_parser(default_tag_parser_options):
-    return TagCommitParser(options=default_tag_parser_options)
+@pytest.fixture(scope="session")
+def default_tag_parser_options(default_tag_parser: TagCommitParser) -> TagParserOptions:
+    return default_tag_parser.get_default_options()
