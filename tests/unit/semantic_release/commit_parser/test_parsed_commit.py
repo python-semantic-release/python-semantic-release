06934621fdccd787d3221d7b838b4ce21cdcbe61
codejedi365 <codejedi365@gmail.com>
2024-04-27 18:31:24 -0400

    test(parser): adapt tests to new create commit fixture

---
+++ tests/unit/semantic_release/commit_parser/test_parsed_commit.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from semantic_release import LevelBump
+from semantic_release.commit_parser import ParsedCommit
+
+if TYPE_CHECKING:
+    from tests.conftest import MakeCommitObjFn
+
+
+def test_parsed_commit_computed_properties(make_commit_obj: MakeCommitObjFn):
+    message = "feat(parser): Add new parser pattern"
+    commit = make_commit_obj(message)
+
+    parsed_commit = ParsedCommit(
+        bump=LevelBump.MINOR,
+        type="feature",
+        scope="parser",
+        descriptions=["Add new parser pattern"],
+        breaking_descriptions=[],
+        commit=commit,
+    )
+
+    assert message == parsed_commit.message
+    assert commit.hexsha == parsed_commit.hexsha
+    assert commit.hexsha[:7] == parsed_commit.short_hash
