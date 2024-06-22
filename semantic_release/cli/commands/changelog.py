498d114a150cf6a4ce61ad83f9218b2b7e2d1078
codejedi365 <codejedi365@gmail.com>
2024-06-22 12:49:39 -0400

    refactor(changelog-cmd): improve readability & logical flow

--- semantic_release/cli/commands/changelog.py
+++ semantic_release/cli/commands/changelog.py
@@ -17,10 +17,74 @@ from semantic_release.cli.util import noop_report
 from semantic_release.hvcs.remote_hvcs_base import RemoteHvcsBase
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
+    from jinja2 import Environment
+
     from semantic_release.cli.commands.cli_context import CliContextObj
-    from semantic_release.version import Version
+
 
 log = logging.getLogger(__name__)
+
+
+def apply_default_changelog(
+    changelog_file: Path,
+    destination_dir: Path,
+    environment: Environment,
+    noop: bool = False,
+) -> None:
+    if noop:
+        noop_report(
+            str.join(" ", [
+                "would have written your changelog to",
+                str(changelog_file.relative_to(destination_dir)),
+            ])
+        )
+        return
+
+    changelog_text = render_default_changelog_file(environment)
+    changelog_file.write_text(f"{changelog_text}\n", encoding="utf-8")
+
+
+def apply_user_changelog_template_directory(
+    template_dir: Path,
+    environment: Environment,
+    target_dir: Path,
+    noop: bool = False,
+) -> None:
+    if noop:
+        noop_report(
+            str.join(" ", [
+                "would have recursively rendered the template directory",
+                repr(template_dir),
+                "relative to",
+                repr(target_dir),
+            ])
+        )
+        return
+
+    recursive_render(template_dir, environment=environment, _root_dir=target_dir)
+
+
+def post_release_notes(
+    release_tag: str,
+    release_notes: str,
+    prerelease: bool,
+    hvcs_client: RemoteHvcsBase,
+    noop: bool = False,
+) -> None:
+    if noop:
+        noop_report(
+            str.join("\n", [
+                f"would have posted the following release notes for tag {release_tag}:",
+                release_notes,
+            ])
+        )
+        return
+
+    hvcs_client.create_or_update_release(
+        release_tag, f"{release_notes}\n", prerelease=prerelease
+    )
 
 
 @click.command(
@@ -36,12 +100,11 @@ log = logging.getLogger(__name__)
     help="Post the generated release notes to the remote VCS's release for this tag",
 )
 @click.pass_obj
-def changelog(cli_ctx: CliContextObj, release_tag: str | None = None) -> None:
+def changelog(cli_ctx: CliContextObj, release_tag: str | None) -> None:
     """Generate and optionally publish a changelog for your project"""
     ctx = click.get_current_context()
     runtime = cli_ctx.runtime_ctx
     repo = runtime.repo
-    parser = runtime.commit_parser
     translator = runtime.version_translator
     hvcs_client = runtime.hvcs_client
     env = runtime.template_environment
@@ -49,73 +112,74 @@ def changelog(cli_ctx: CliContextObj, release_tag: str | None = None) -> None:
     changelog_file = runtime.changelog_file
     changelog_excluded_commit_patterns = runtime.changelog_excluded_commit_patterns
 
-    rh = ReleaseHistory.from_git_history(
+    release_history = ReleaseHistory.from_git_history(
         repo=repo,
         translator=translator,
-        commit_parser=parser,
+        commit_parser=runtime.commit_parser,
         exclude_commit_patterns=changelog_excluded_commit_patterns,
     )
+
     changelog_context = make_changelog_context(
-        hvcs_client=hvcs_client, release_history=rh
+        hvcs_client=hvcs_client, release_history=release_history,
     )
+
     changelog_context.bind_to_environment(env)
 
-    if not os.path.exists(template_dir):
-        log.info("Path %r not found, using default changelog template", template_dir)
-        if runtime.global_cli_options.noop:
-            noop_report(
-                "would have written your changelog to "
-                + str(changelog_file.relative_to(repo.working_dir))
-            )
-        else:
-            changelog_text = render_default_changelog_file(env)
-            changelog_file.write_text(f"{changelog_text}\n", encoding="utf-8")
 
+    if template_dir.exists():
+        apply_user_changelog_template_directory(
+            template_dir=template_dir,
+            environment=env,
+            target_dir=Path(repo.working_dir),
+            noop=runtime.global_cli_options.noop,
+        )
     else:
-        if runtime.global_cli_options.noop:
-            noop_report(
-                f"would have recursively rendered the template directory "
-                f"{template_dir!r} relative to {repo.working_dir!r}"
-            )
-        else:
-            recursive_render(template_dir, environment=env, _root_dir=repo.working_dir)
-
-    if release_tag and isinstance(hvcs_client, RemoteHvcsBase):
-        if runtime.global_cli_options.noop:
-            noop_report(
-                f"would have posted changelog to the release for tag {release_tag}"
-            )
-
-        # note: the following check ensures 'version is not None', but mypy can't follow
-        version: Version = translator.from_tag(release_tag)  # type: ignore[assignment]
-        if not version:
-            ctx.fail(
-                f"Tag {release_tag!r} doesn't match tag format "
-                f"{translator.tag_format!r}"
-            )
-
-        try:
-            release = rh.released[version]
-        except KeyError:
-            ctx.fail(f"tag {release_tag} not in release history")
-
-        template = get_release_notes_template(template_dir)
-        release_notes = render_release_notes(
-            release_notes_template=template,
-            template_environment=env,
-            version=version,
-            release=release,
+        log.info("No contents found in %r, using default changelog template", template_dir)
+        apply_default_changelog(
+            changelog_file=changelog_file,
+            destination_dir=Path(repo.working_dir),
+            environment=env,
+            noop=runtime.global_cli_options.noop,
         )
 
-        if runtime.global_cli_options.noop:
-            noop_report(
-                "would have posted the following release notes:\n" + release_notes
-            )
-        else:
-            try:
-                hvcs_client.create_or_update_release(
-                    release_tag, f"{release_notes}\n", prerelease=version.is_prerelease
-                )
-            except Exception as e:
-                log.exception(e)
-                ctx.fail(str(e))
+    if not release_tag:
+        return
+
+    if not isinstance(hvcs_client, RemoteHvcsBase):
+        log.info(
+            "Remote does not support releases. Skipping release notes update..."
+        )
+        return
+
+    if not (version := translator.from_tag(release_tag)):
+        ctx.fail(
+            str.join(" ", [
+                f"Tag {release_tag!r} does not match the tag format",
+                repr(translator.tag_format),
+            ])
+        )
+
+    try:
+        release = release_history.released[version]
+    except KeyError:
+        ctx.fail(f"tag {release_tag} not in release history")
+
+    template = get_release_notes_template(template_dir)
+    release_notes = render_release_notes(
+        release_notes_template=template,
+        template_environment=env,
+        version=version,
+        release=release,
+    )
+
+    try:
+        post_release_notes(
+            release_tag=release_tag,
+            release_notes=release_notes,
+            prerelease=version.is_prerelease,
+            hvcs_client=hvcs_client,
+            noop=runtime.global_cli_options.noop,
+        )
+    except Exception as e:
+        log.exception(e)
+        ctx.fail(str(e))
