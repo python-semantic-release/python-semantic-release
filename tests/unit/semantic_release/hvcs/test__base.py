dea288728e62092237b38ac5888756d5de96404e
codejedi365 <codejedi365@gmail.com>
2024-06-30 00:24:33 -0400

    style(test): improve maintainability via fixture references & typing

--- tests/unit/semantic_release/hvcs/test__base.py
+++ tests/unit/semantic_release/hvcs/test__base.py
@@ -8,6 +8,7 @@ from pytest_lazyfixture import lazy_fixture
 from semantic_release.hvcs._base import HvcsBase
 
 from tests.const import EXAMPLE_REPO_NAME, EXAMPLE_REPO_OWNER
+from tests.fixtures.git_repo import example_git_https_url, example_git_ssh_url
 
 if TYPE_CHECKING:
     from typing import Any, Callable
@@ -24,8 +25,8 @@ class ArbitraryHvcs(HvcsBase):
 @pytest.mark.parametrize(
     "remote_url, repo_name",
     [
-        (lazy_fixture("example_git_ssh_url"), EXAMPLE_REPO_NAME),
-        (lazy_fixture("example_git_https_url"), EXAMPLE_REPO_NAME),
+        (lazy_fixture(example_git_ssh_url.__name__), EXAMPLE_REPO_NAME),
+        (lazy_fixture(example_git_https_url.__name__), EXAMPLE_REPO_NAME),
         ("git@my.corp.custom.domain:very_serious/business.git", "business"),
     ],
 )
@@ -37,8 +38,8 @@ def test_get_repository_owner(remote_url, repo_name):
 @pytest.mark.parametrize(
     "remote_url, owner",
     [
-        (lazy_fixture("example_git_ssh_url"), EXAMPLE_REPO_OWNER),
-        (lazy_fixture("example_git_https_url"), EXAMPLE_REPO_OWNER),
+        (lazy_fixture(example_git_ssh_url.__name__), EXAMPLE_REPO_OWNER),
+        (lazy_fixture(example_git_https_url.__name__), EXAMPLE_REPO_OWNER),
         ("git@my.corp.custom.domain:very_serious/business.git", "very_serious"),
     ],
 )
