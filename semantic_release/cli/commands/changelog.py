809c6075be4f2179a32d18c51ef3390c4185ccc5
codejedi365 <codejedi365@gmail.com>
2024-06-30 00:42:37 -0400

    refactor(changelog-cmd): simplify and improve implementation

--- semantic_release/cli/commands/changelog.py
+++ semantic_release/cli/commands/changelog.py
@@ -1,69 +1,23 @@
 from __future__ import annotations
 
 import logging
-import os
 from typing import TYPE_CHECKING
 
 import click
 
-from semantic_release.changelog import ReleaseHistory, recursive_render
-from semantic_release.changelog.context import make_changelog_context
-from semantic_release.cli.common import (
-    get_release_notes_template,
-    render_default_changelog_file,
-    render_release_notes,
+from semantic_release.changelog import ReleaseHistory
+from semantic_release.cli.changelog_writer import (
+    generate_release_notes,
+    write_changelog_files,
 )
 from semantic_release.cli.util import noop_report
 from semantic_release.hvcs.remote_hvcs_base import RemoteHvcsBase
 
 if TYPE_CHECKING:
-    from pathlib import Path
-
-    from jinja2 import Environment
-
     from semantic_release.cli.cli_context import CliContextObj
 
 
 log = logging.getLogger(__name__)
-
-
-def apply_default_changelog(
-    changelog_file: Path,
-    destination_dir: Path,
-    environment: Environment,
-    noop: bool = False,
-) -> None:
-    if noop:
-        noop_report(
-            str.join(" ", [
-                "would have written your changelog to",
-                str(changelog_file.relative_to(destination_dir)),
-            ])
-        )
-        return
-
-    changelog_text = render_default_changelog_file(environment)
-    changelog_file.write_text(f"{changelog_text}\n", encoding="utf-8")
-
-
-def apply_user_changelog_template_directory(
-    template_dir: Path,
-    environment: Environment,
-    target_dir: Path,
-    noop: bool = False,
-) -> None:
-    if noop:
-        noop_report(
-            str.join(" ", [
-                "would have recursively rendered the template directory",
-                repr(template_dir),
-                "relative to",
-                repr(target_dir),
-            ])
-        )
-        return
-
-    recursive_render(template_dir, environment=environment, _root_dir=target_dir)
 
 
 def post_release_notes(
@@ -107,49 +61,22 @@ def changelog(cli_ctx: CliContextObj, release_tag: str | None) -> None:
     """Generate and optionally publish a changelog for your project"""
     ctx = click.get_current_context()
     runtime = cli_ctx.runtime_ctx
-    repo = runtime.repo
     translator = runtime.version_translator
     hvcs_client = runtime.hvcs_client
-    env = runtime.template_environment
-    template_dir = runtime.template_dir
-    changelog_file = runtime.changelog_file
-    changelog_excluded_commit_patterns = runtime.changelog_excluded_commit_patterns
 
     release_history = ReleaseHistory.from_git_history(
-        repo=repo,
+        repo=runtime.repo,
         translator=translator,
         commit_parser=runtime.commit_parser,
-        exclude_commit_patterns=changelog_excluded_commit_patterns,
+        exclude_commit_patterns=runtime.changelog_excluded_commit_patterns,
     )
 
-    changelog_context = make_changelog_context(
-        hvcs_client=hvcs_client, release_history=release_history,
+    write_changelog_files(
+        runtime_ctx=runtime,
+        release_history=release_history,
+        hvcs_client=hvcs_client,
+        noop=runtime.global_cli_options.noop,
     )
-
-    changelog_context.bind_to_environment(env)
-
-    use_user_template_dir = bool(
-        # Directory exists and directory is not empty
-        template_dir.exists()
-        and template_dir.is_dir()
-        and os.listdir(template_dir)
-    )
-
-    if use_user_template_dir:
-        apply_user_changelog_template_directory(
-            template_dir=template_dir,
-            environment=env,
-            target_dir=Path(repo.working_dir),
-            noop=runtime.global_cli_options.noop,
-        )
-    else:
-        log.info("No contents found in %r, using default changelog template", template_dir)
-        apply_default_changelog(
-            changelog_file=changelog_file,
-            destination_dir=Path(repo.working_dir),
-            environment=env,
-            noop=runtime.global_cli_options.noop,
-        )
 
     if not release_tag:
         return
@@ -174,12 +101,10 @@ def changelog(cli_ctx: CliContextObj, release_tag: str | None) -> None:
     except KeyError:
         ctx.fail(f"tag {release_tag} not in release history")
 
-    template = get_release_notes_template(template_dir)
-    release_notes = render_release_notes(
-        release_notes_template=template,
-        template_environment=env,
-        version=version,
-        release=release,
+    release_notes = generate_release_notes(
+        hvcs_client,
+        release,
+        template_dir=runtime.template_dir,
     )
 
     try:
