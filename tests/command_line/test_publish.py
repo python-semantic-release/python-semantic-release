780227156eff08489af5ed11324db0cb42607984
codejedi365 <codejedi365@gmail.com>
2024-06-30 00:11:01 -0400

    test(cli-publish): refactor for improved testing

--- tests/command_line/test_publish.py
+++ tests/command_line/test_publish.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 from unittest import mock
 
 import pytest
@@ -6,32 +9,68 @@ from semantic_release.cli.commands.main import main
 from semantic_release.hvcs import Github
 
 from tests.const import MAIN_PROG_NAME, PUBLISH_SUBCMD
+from tests.fixtures.repos import repo_with_single_branch_angular_commits
 from tests.util import assert_exit_code, assert_successful_exit_code
+
+if TYPE_CHECKING:
+    from typing import Sequence
+
+    from click.testing import CliRunner
+
+    from tests.fixtures.git_repo import GetVersionStringsFn
+
+
 @pytest.mark.parametrize("cmd_args", [(), ("--tag", "latest")])
+@pytest.mark.usefixtures(repo_with_single_branch_angular_commits.__name__)
 def test_publish_latest_uses_latest_tag(
-    cli_runner,
-    cmd_args,
+    cli_runner: CliRunner,
+    cmd_args: Sequence[str],
+    get_versions_for_trunk_only_repo_w_tags: GetVersionStringsFn,
 ):
-    with mock.patch.object(Github, "upload_dists") as mocked_upload_dists, mock.patch(
-        "semantic_release.cli.commands.publish.tags_and_versions",
-        return_value=([("v1.0.0", "1.0.0")]),
-    ) as mock_tags_and_versions:
-        result = cli_runner.invoke(main, [publish.name, *cmd_args])
-        assert result.exit_code == 0
+    latest_tag = f"v{get_versions_for_trunk_only_repo_w_tags()[-1]}"
+    with mock.patch.object(
+        Github,
+        Github.upload_dists.__name__,
+    ) as mocked_upload_dists:
+        cli_cmd = [MAIN_PROG_NAME, PUBLISH_SUBCMD, *cmd_args]
 
-    mock_tags_and_versions.assert_called_once()
-    mocked_upload_dists.assert_called_once_with(tag="v1.0.0", dist_glob="dist/*")
+        # Act
+        result = cli_runner.invoke(main, cli_cmd[1:])
+
+        # Evaluate
+        assert_successful_exit_code(result, cli_cmd)
+        mocked_upload_dists.assert_called_once_with(tag=latest_tag, dist_glob="dist/*")
 
 
-@pytest.mark.usefixtures("repo_with_single_branch_angular_commits")
-def test_publish_to_tag_uses_tag(cli_runner):
-    tag = "v99.999.9999+build.1234"
-    with mock.patch.object(Github, "upload_dists") as mocked_upload_dists, mock.patch(
-        "semantic_release.cli.commands.publish.tags_and_versions",
-        return_value=([("v1.0.0", "1.0.0")]),
-    ) as mock_tags_and_versions:
-        result = cli_runner.invoke(main, [publish.name, "--tag", tag])
-        assert result.exit_code == 0
+@pytest.mark.usefixtures(repo_with_single_branch_angular_commits.__name__)
+def test_publish_to_tag_uses_tag(
+    cli_runner: CliRunner,
+    get_versions_for_trunk_only_repo_w_tags: GetVersionStringsFn,
+):
+    # Testing a non-latest tag to distinguish from test_publish_latest_uses_latest_tag()
+    previous_tag = f"v{get_versions_for_trunk_only_repo_w_tags()[-2]}"
 
-    mock_tags_and_versions.assert_not_called()
-    mocked_upload_dists.assert_called_once_with(tag=tag, dist_glob="dist/*")
+    with mock.patch.object(Github, Github.upload_dists.__name__) as mocked_upload_dists:
+        cli_cmd = [MAIN_PROG_NAME, PUBLISH_SUBCMD, "--tag", previous_tag]
+
+        # Act
+        result = cli_runner.invoke(main, cli_cmd[1:])
+
+        # Evaluate
+        assert_successful_exit_code(result, cli_cmd)
+        mocked_upload_dists.assert_called_once_with(
+            tag=previous_tag, dist_glob="dist/*"
+        )
+
+
+@pytest.mark.usefixtures(repo_with_single_branch_angular_commits.__name__)
+def test_publish_fails_on_nonexistant_tag(cli_runner: CliRunner):
+    with mock.patch.object(Github, Github.upload_dists.__name__) as mocked_upload_dists:
+        cli_cmd = [MAIN_PROG_NAME, PUBLISH_SUBCMD, "--tag", "nonexistant-tag"]
+
+        # Act
+        result = cli_runner.invoke(main, cli_cmd[1:])
+
+        # Evaluate
+        assert_exit_code(1, result, cli_cmd)
+        mocked_upload_dists.assert_not_called()
