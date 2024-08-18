c226c51f2ad2394e196c6b90061608d410b98854
codejedi365 <codejedi365@gmail.com>
2024-08-18 17:33:39 -0600

    fix(release-notes): fix noop-changelog to print raw release notes
    Some markdown sequences can be interpreted as ansi escape sequences which dilute
    debugging of release note templates by the user. This change ensures the raw
    content is displayed to the console as expected.

--- semantic_release/cli/commands/changelog.py
+++ semantic_release/cli/commands/changelog.py
@@ -34,7 +34,9 @@ def post_release_notes(
                 "\n",
                 [
                     f"would have posted the following release notes for tag {release_tag}:",
-                    release_notes,
+                    # Escape square brackets to ensure all content is displayed in the console
+                    # (i.e. prevent interpretation of ansi escape sequences that is valid markdown)
+                    release_notes.replace("[", "\\["),
                 ],
             )
         )
