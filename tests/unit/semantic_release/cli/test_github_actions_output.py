659d97329ed93758f65e59a6b9719a374d203dd5
codejedi365 <codejedi365@gmail.com>
2024-09-27 23:41:06 -0600

    test(github-actions): add test to ensure `is_prerelease` is a action output

--- tests/unit/semantic_release/cli/test_github_actions_output.py
+++ tests/unit/semantic_release/cli/test_github_actions_output.py
@@ -14,19 +14,28 @@ if TYPE_CHECKING:
     from pathlib import Path
 
 
+@pytest.mark.parametrize(
+    "version, is_prerelease",
+    [
+        ("1.2.3", False),
+        ("1.2.3-alpha.1", True),
+    ],
+)
 @pytest.mark.parametrize("released", (True, False))
-def test_version_github_actions_output_format(released: bool):
-    version_str = "1.2.3"
+def test_version_github_actions_output_format(
+    released: bool, version: str, is_prerelease: bool
+):
     expected_output = dedent(
         f"""\
         released={'true' if released else 'false'}
-        version={version_str}
-        tag=v{version_str}
+        version={version}
+        tag=v{version}
+        is_prerelease={'true' if is_prerelease else 'false'}
         """
     )
     output = VersionGitHubActionsOutput(
         released=released,
-        version=Version.parse(version_str),
+        version=Version.parse(version),
     )
 
     # Evaluate (expected -> actual)
@@ -63,6 +72,7 @@ def test_version_github_actions_output_writes_to_github_output_if_available(
     # Evaluate (expected -> actual)
     assert version_str == action_outputs["version"]
     assert str(True).lower() == action_outputs["released"]
+    assert str(False).lower() == action_outputs["is_prerelease"]
 
 
 def test_version_github_actions_output_no_error_if_not_in_gha(
