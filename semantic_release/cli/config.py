dd693bac57a660e4aa1fdc46bb6a5bbed30a43cc
codejedi365 <codejedi365@gmail.com>
2024-09-30 23:24:47 -0600

    feat(config): add `changelog.default_templates.output_format` config option

--- semantic_release/cli/config.py
+++ semantic_release/cli/config.py
@@ -95,6 +95,13 @@ class EnvConfigVar(BaseModel):
 MaybeFromEnv = Union[EnvConfigVar, str]
 
 
+class ChangelogOutputFormat(str, Enum):
+    """Supported changelog output formats when using the default templates."""
+
+    MARKDOWN = "md"
+    # RESTRUCTURED_TEXT = "rst"
+
+
 class ChangelogEnvironmentConfig(BaseModel):
     block_start_string: str = "{%"
     block_end_string: str = "%}"
@@ -112,8 +119,18 @@ class ChangelogEnvironmentConfig(BaseModel):
     autoescape: Union[bool, str] = False
 
 
+class DefaultChangelogTemplatesConfig(BaseModel):
+    # TODO: BREAKING CHANGE v10
+    # changelog_file: str = "CHANGELOG.md"
+    output_format: ChangelogOutputFormat = ChangelogOutputFormat.MARKDOWN
+
+
 class ChangelogConfig(BaseModel):
+    # TODO: BREAKING CHANGE v10, move to DefaultChangelogTemplatesConfig
     changelog_file: str = "CHANGELOG.md"
+    default_templates: DefaultChangelogTemplatesConfig = (
+        DefaultChangelogTemplatesConfig()
+    )
     environment: ChangelogEnvironmentConfig = ChangelogEnvironmentConfig()
     exclude_commit_patterns: Tuple[str, ...] = ()
     mode: ChangelogMode = ChangelogMode.INIT
@@ -392,6 +409,8 @@ class RuntimeContext:
     changelog_insertion_flag: str
     changelog_mode: ChangelogMode
     changelog_file: Path
+    changelog_style: str
+    changelog_output_format: ChangelogOutputFormat
     ignore_token_for_push: bool
     template_environment: Environment
     template_dir: Path
@@ -645,6 +664,18 @@ class RuntimeContext:
 
             build_cmd_env[name] = env_val
 
+        # TODO: better support for custom parsers that actually just extend defaults
+        #
+        # Here we just assume the desired changelog style matches the parser name
+        # as we provide templates specific to each parser type. Unfortunately if the user has
+        # provided a custom parser, it would be up to the user to provide custom templates
+        # but we just assume the base template is angular
+        # changelog_style = (
+        #     raw.commit_parser
+        #     if raw.commit_parser in _known_commit_parsers
+        #     else "angular"
+        # )
+
         self = cls(
             repo_dir=raw.repo_dir,
             commit_parser=commit_parser,
@@ -662,6 +693,10 @@ class RuntimeContext:
             commit_author=commit_author,
             commit_message=raw.commit_message,
             changelog_excluded_commit_patterns=changelog_excluded_commit_patterns,
+            # TODO: change when we have other styles per parser
+            # changelog_style=changelog_style,
+            changelog_style="angular",
+            changelog_output_format=raw.changelog.default_templates.output_format,
             prerelease=branch_config.prerelease,
             ignore_token_for_push=raw.remote.ignore_token_for_push,
             template_dir=template_dir,
