13ca44f4434098331f70e6937684679cf1b4106a
codejedi365 <codejedi365@users.noreply.github.com>
2024-11-06 17:08:44 -0700

    fix(cli): gracefully capture all exceptions unless in very verbose debug mode (#1088)
    * refactor(cli): consolidate entrypoints into the module execute file

--- src/semantic_release/__main__.py
+++ src/semantic_release/__main__.py
@@ -1,6 +1,42 @@
-import sys
+"""Entrypoint for the `semantic-release` module."""
+# ruff: noqa: T201, print statements are fine here as this is for cli entry only
 
-from semantic_release.cli.commands.main import main
+from __future__ import annotations
+
+import sys
+from traceback import format_exception
+
+from semantic_release import globals
+from semantic_release.cli.commands.main import main as cli_main
+
+
+def main() -> None:
+    try:
+        cli_main(args=sys.argv[1:])
+        print("semantic-release completed successfully.", file=sys.stderr)
+    except KeyboardInterrupt:
+        print("\n-- User Abort! --", file=sys.stderr)
+        sys.exit(127)
+    except Exception as err:  # noqa: BLE001, graceful error handling across application
+        if globals.debug:
+            print(f"{err.__class__.__name__}: {err}\n", file=sys.stderr)
+            etype, value, traceback = sys.exc_info()
+            print(
+                str.join(
+                    "",
+                    format_exception(
+                        etype,
+                        value,
+                        traceback,
+                        limit=None,
+                        chain=True,
+                    )[:-1],
+                ),
+                file=sys.stderr,
+            )
+        print(f"::ERROR:: {err}", file=sys.stderr)
+        sys.exit(1)
+
 
 if __name__ == "__main__":
-    main(args=sys.argv[1:])
+    main()
---
+++ src/semantic_release/globals.py
@@ -0,0 +1,6 @@
+"""Semantic Release Global Variables."""
+
+from __future__ import annotations
+
+debug: bool = False
+"""bool: Enable debug level logging and runtime actions."""
