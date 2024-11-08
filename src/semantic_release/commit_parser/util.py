436374b04128d1550467ae97ba90253f1d1b3878
codejedi365 <codejedi365@gmail.com>
2024-11-08 13:25:27 -0700

    fix(parsers): improve reliability of text unwordwrap of descriptions

--- src/semantic_release/commit_parser/util.py
+++ src/semantic_release/commit_parser/util.py
@@ -1,8 +1,33 @@
 from __future__ import annotations
 
-import re
+from functools import reduce
+from re import compile as regexp
+from typing import TYPE_CHECKING
 
-breaking_re = re.compile(r"BREAKING[ -]CHANGE:\s?(.*)")
+if TYPE_CHECKING:
+    from re import Pattern
+    from typing import TypedDict
+
+    class RegexReplaceDef(TypedDict):
+        pattern: Pattern
+        repl: str
+
+
+breaking_re = regexp(r"BREAKING[ -]CHANGE:\s?(.*)")
+un_word_wrap: RegexReplaceDef = {
+    # Match a line ending where the next line is not indented, or a bullet
+    "pattern": regexp(r"((?<!-)\n(?![\s*-]))"),
+    "repl": r" ",  # Replace with a space
+}
+un_word_wrap_hyphen: RegexReplaceDef = {
+    "pattern": regexp(r"((?<=\w)-\n(?=\w))"),
+    "repl": r"-",  # Replace with single hyphen
+}
+trim_line_endings: RegexReplaceDef = {
+    # Match line endings with optional whitespace
+    "pattern": regexp(r"[\r\t\f\v ]*\r?\n"),
+    "repl": "\n",  # remove the optional whitespace & remove windows newlines
+}
 
 
 def parse_paragraphs(text: str) -> list[str]:
@@ -16,12 +41,18 @@ def parse_paragraphs(text: str) -> list[str]:
     :param text: The text string to be divided.
     :return: A list of condensed paragraphs, as strings.
     """
+    adjusted_text = reduce(
+        lambda txt, adj: adj["pattern"].sub(adj["repl"], txt),
+        [trim_line_endings, un_word_wrap_hyphen],
+        text,
+    )
+
     return list(
         filter(
             None,
             [
-                paragraph.replace("\n", " ").strip()
-                for paragraph in text.replace("\r", "").split("\n\n")
+                un_word_wrap["pattern"].sub(un_word_wrap["repl"], paragraph).strip()
+                for paragraph in adjusted_text.split("\n\n")
             ],
         )
     )
