3c08dfcd7b29459472cdebddf47f3f4a58bbf8e5
codejedi365 <codejedi365@gmail.com>
2024-11-11 14:39:31 -0700

    feat(parser-scipy)!: set parser to evaluate all squashed commits by default
    BREAKING CHANGE: The configuration setting `commit_parser_options.parse_squash_commits`
    is now set to `true` by default. The feature to parse squash commits was introduced in
    `v9.17.0` and was originally set to `false` to prevent unexpected results on a non-breaking
    update. The parse squash commits feature attempts to find additional commits of the same
    commit type within the body of a single commit message. When squash commits are found,
    Python Semantic Release will separate out each commit into its own artificial commit object
    and parse them individually. This potentially can change the resulting version bump if
    a larger bump was detected within the squashed components. It also allows for the changelog
    and release notes to separately order and display each commit as originally written. If
    this is not desired, you will need to update your configuration to change the new setting
    to `false`.

--- src/semantic_release/commit_parser/scipy.py
+++ src/semantic_release/commit_parser/scipy.py
@@ -147,8 +147,7 @@ class ScipyParserOptions(ParserOptions):
     default_level_bump: LevelBump = LevelBump.NO_RELEASE
     """The minimum bump level to apply to valid commit message."""
 
-    # TODO: breaking change v10, change default to True
-    parse_squash_commits: bool = False
+    parse_squash_commits: bool = True
     """Toggle flag for whether or not to parse squash commits"""
 
     # TODO: breaking change v10, change default to True
