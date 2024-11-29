018914a4cdcc3c63c979e0e443481fda52fb9389
codejedi365 <codejedi365@gmail.com>
2024-11-29 10:53:12 -0700

    chore(tests): force unit tests order to be first in case of `--comprehensive`

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -12,3 +12,5 @@ def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
     for item in items:
         if unit_test_directory in item.path.parents:
             item.add_marker(pytest.mark.unit)
+            if "order" not in [mark.name for mark in item.own_markers]:
+                item.add_marker(pytest.mark.order("first"))
