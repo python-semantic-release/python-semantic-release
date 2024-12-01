88c9ca44228947dc0959f833a394fe9f8a06f903
codejedi365 <codejedi365@gmail.com>
2024-11-30 23:29:08 -0700

    test(parser-angular): add unit tests to verify parsing of issue numbers

--- tests/unit/semantic_release/commit_parser/test_angular.py
+++ tests/unit/semantic_release/commit_parser/test_angular.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 
 import pytest
 
@@ -10,6 +10,8 @@ from semantic_release.commit_parser.angular import (
 )
 from semantic_release.commit_parser.token import ParsedCommit, ParseError
 from semantic_release.enums import LevelBump
+
+from tests.const import SUPPORTED_ISSUE_CLOSURE_PREFIXES
 
 if TYPE_CHECKING:
     from tests.conftest import MakeCommitObjFn
@@ -112,7 +114,7 @@ _long_text = (
     "This is an long explanatory part of a commit message. It should give "
     "some insight to the fix this commit adds to the codebase."
 )
-_footer = "Closes #400"
+_footer = "Closes: #400"
 
 
 @pytest.mark.parametrize(
@@ -183,6 +185,280 @@ def test_parser_return_linked_merge_request_from_commit_message(
     assert isinstance(result, ParsedCommit)
     assert merge_request_number == result.linked_merge_request
     assert subject == result.descriptions[0]
+
+
+@pytest.mark.parametrize(
+    "message, linked_issues",
+    # TODO: in v10, we will remove the issue reference footers from the descriptions
+    [
+        *[
+            # GitHub, Gitea, GitLab style
+            (
+                f"feat(parser): add magic parser\n\n{footer}",
+                linked_issues,
+            )
+            for footer_prefix in SUPPORTED_ISSUE_CLOSURE_PREFIXES
+            for footer, linked_issues in [
+                # Single issue
+                (
+                    f"{footer_prefix.capitalize()}: #555",
+                    ["#555"],
+                ),  # Git Footer style (capitalized)
+                (f"{footer_prefix.lower()}: #555", ["#555"]),  # lowercase prefix
+                (f"{footer_prefix.upper()}: #555", ["#555"]),  # uppercase prefix
+                # Mulitple issues (variant 1: list with one prefix, not supported by GitHub)
+                (
+                    f"{footer_prefix}: #555,#444",
+                    ["#444", "#555"],
+                ),  # Comma separated (w/o space)
+                (
+                    f"{footer_prefix}: #555, #444",
+                    ["#444", "#555"],
+                ),  # Comma separated (w/ space)
+                (
+                    f"{footer_prefix}: #555 , #444",
+                    ["#444", "#555"],
+                ),  # Comma separated (w/ extra space)
+                (f"{footer_prefix}: #555 #444", ["#444", "#555"]),  # Space separated
+                (
+                    f"{footer_prefix}: #555;#444",
+                    ["#444", "#555"],
+                ),  # semicolon separated (w/o space)
+                (
+                    f"{footer_prefix}: #555; #444",
+                    ["#444", "#555"],
+                ),  # semicolon separated (w/ space)
+                (
+                    f"{footer_prefix}: #555 ; #444",
+                    ["#444", "#555"],
+                ),  # semicolon separated (w/ extra space)
+                (
+                    f"{footer_prefix}: #555/#444",
+                    ["#444", "#555"],
+                ),  # slash separated (w/o space)
+                (
+                    f"{footer_prefix}: #555/ #444",
+                    ["#444", "#555"],
+                ),  # slash separated (w/ space)
+                (
+                    f"{footer_prefix}: #555 / #444",
+                    ["#444", "#555"],
+                ),  # slash separated (w/ extra space)
+                (
+                    f"{footer_prefix}: #555&#444",
+                    ["#444", "#555"],
+                ),  # ampersand separated (w/o space)
+                (
+                    f"{footer_prefix}: #555& #444",
+                    ["#444", "#555"],
+                ),  # ampersand separated (w/ space)
+                (
+                    f"{footer_prefix}: #555 & #444",
+                    ["#444", "#555"],
+                ),  # ampersand separated (w/ extra space)
+                (f"{footer_prefix}: #555 and #444", ["#444", "#555"]),  # and separated
+                (
+                    f"{footer_prefix}: #555, #444, and #333",
+                    ["#333", "#444", "#555"],
+                ),  # and separated
+                # Mulitple issues (variant 2: multiple footers, supported by GitHub)
+                (f"{footer_prefix}: #555\n{footer_prefix}: #444", ["#444", "#555"]),
+                # More than 2 issues
+                (
+                    f"{footer_prefix}: #555, #444, #333",
+                    ["#333", "#444", "#555"],
+                ),  # TODO: force ordering?
+                # Single issue listed multiple times
+                (f"{footer_prefix}: #555, #555", ["#555"]),
+                # Multiple footers with the same issue
+                (f"{footer_prefix}: #555\n{footer_prefix}: #555", ["#555"]),
+                # Multiple issues via multiple inline git footers
+                (f"{footer_prefix}: #555, {footer_prefix}: #444", ["#444", "#555"]),
+                # Multiple valid footers
+                (
+                    str.join(
+                        "\n",
+                        [
+                            f"{footer_prefix}: #555",
+                            "Signed-off-by: johndoe <johndoe@mail.com>",
+                            f"{footer_prefix}: #444",
+                        ],
+                    ),
+                    ["#444", "#555"],
+                ),
+                # ----------------------------------------- Invalid Sets ----------------------------------------- #
+                # Must have colon because it is a git footer, these will not return a linked issue
+                (f"{footer_prefix} #666", []),
+                (f"{footer_prefix} #666, #777", []),
+                # Invalid Multiple issues (although it is supported by GitHub, it is not supported by the parser)
+                (f"{footer_prefix} #666, {footer_prefix} #777", []),
+                # Invalid 'and' separation
+                (f"{footer_prefix}: #666and#777", ["#666and#777"]),
+                # Invalid prefix
+                ("ref: #666", []),
+                # body mentions an issue and has a different git footer
+                (
+                    "In #666, the devils in the details...\n\nSigned-off-by: johndoe <johndoe@mail.com>",
+                    [],
+                ),
+            ]
+        ],
+        *[
+            # JIRA style
+            (
+                f"feat(parser): add magic parser\n\n{footer}",
+                linked_issues,
+            )
+            for footer_prefix in SUPPORTED_ISSUE_CLOSURE_PREFIXES
+            for footer, linked_issues in [
+                # Single issue
+                (
+                    f"{footer_prefix.capitalize()}: ABC-555",
+                    ["ABC-555"],
+                ),  # Git Footer style (capitalized)
+                (f"{footer_prefix.lower()}: ABC-555", ["ABC-555"]),  # lowercase prefix
+                (f"{footer_prefix.upper()}: ABC-555", ["ABC-555"]),  # uppercase prefix
+                # Mulitple issues (variant 1: list with one prefix, not supported by GitHub)
+                (
+                    f"{footer_prefix}: ABC-555,ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # Comma separated (w/o space)
+                (
+                    f"{footer_prefix}: ABC-555, ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # Comma separated (w/ space)
+                (
+                    f"{footer_prefix}: ABC-555 , ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # Comma separated (w/ extra space)
+                (
+                    f"{footer_prefix}: ABC-555 ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # Space separated
+                (
+                    f"{footer_prefix}: ABC-555;ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # semicolon separated (w/o space)
+                (
+                    f"{footer_prefix}: ABC-555; ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # semicolon separated (w/ space)
+                (
+                    f"{footer_prefix}: ABC-555 ; ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # semicolon separated (w/ extra space)
+                (
+                    f"{footer_prefix}: ABC-555/ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # slash separated (w/o space)
+                (
+                    f"{footer_prefix}: ABC-555/ ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # slash separated (w/ space)
+                (
+                    f"{footer_prefix}: ABC-555 / ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # slash separated (w/ extra space)
+                (
+                    f"{footer_prefix}: ABC-555&ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # ampersand separated (w/o space)
+                (
+                    f"{footer_prefix}: ABC-555& ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # ampersand separated (w/ space)
+                (
+                    f"{footer_prefix}: ABC-555 & ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # ampersand separated (w/ extra space)
+                (
+                    f"{footer_prefix}: ABC-555 and ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),  # and separated
+                (
+                    f"{footer_prefix}: ABC-555, ABC-444, and ABC-333",
+                    ["ABC-333", "ABC-444", "ABC-555"],
+                ),  # and separated
+                # Mulitple issues (variant 2: multiple footers, supported by GitHub)
+                (
+                    f"{footer_prefix}: ABC-555\n{footer_prefix}: ABC-444",
+                    ["ABC-444", "ABC-555"],
+                ),
+                # More than 2 issues
+                (
+                    f"{footer_prefix}: ABC-555, ABC-444, ABC-333",
+                    ["ABC-333", "ABC-444", "ABC-555"],
+                ),  # TODO: force ordering?
+                # Single issue listed multiple times
+                (f"{footer_prefix}: ABC-555, ABC-555", ["ABC-555"]),
+                # Multiple footers with the same issue
+                (f"{footer_prefix}: ABC-555\n{footer_prefix}: ABC-555", ["ABC-555"]),
+                # Multiple issues via multiple inline git footers
+                (
+                    f"{footer_prefix}: ABC-666, {footer_prefix}: ABC-777",
+                    ["ABC-666", "ABC-777"],
+                ),
+                # Multiple valid footers
+                (
+                    str.join(
+                        "\n",
+                        [
+                            f"{footer_prefix}: ABC-555",
+                            "Signed-off-by: johndoe <johndoe@mail.com>",
+                            f"{footer_prefix}: ABC-444",
+                        ],
+                    ),
+                    ["ABC-444", "ABC-555"],
+                ),
+                # ----------------------------------------- Invalid Sets ----------------------------------------- #
+                # Must have colon because it is a git footer, these will not return a linked issue
+                (f"{footer_prefix} ABC-666", []),
+                (f"{footer_prefix} ABC-666, ABC-777", []),
+                # Invalid Multiple issues (although it is supported by GitHub, it is not supported by the parser)
+                (f"{footer_prefix} ABC-666, {footer_prefix} ABC-777", []),
+                # Invalid 'and' separation
+                (f"{footer_prefix}: ABC-666andABC-777", ["ABC-666andABC-777"]),
+                # Invalid prefix
+                ("ref: ABC-666", []),
+                # body mentions an issue and has a different git footer
+                (
+                    "In ABC-666, the devils in the details...\n\nSigned-off-by: johndoe <johndoe@mail.com>",
+                    [],
+                ),
+            ]
+        ],
+        *[
+            (
+                f"feat(parser): add magic parser\n\n{footer}",
+                linked_issues,
+            )
+            for footer, linked_issues in [
+                # Multiple footers with the same issue but different prefixes
+                ("Resolves: #555\nfix: #444", ["#444", "#555"]),
+                # Whitespace padded footer
+                ("  Resolves: #555\n", ["#555"]),
+            ]
+        ],
+        (
+            # Only grabs the issue reference when there is a GitHub PR reference in the subject
+            "feat(parser): add magic parser (#123)\n\nCloses: #555",
+            ["#555"],
+        ),
+        # Does not grab an issue when there is only a GitHub PR reference in the subject
+        ("feat(parser): add magic parser (#123)", []),
+        # Does not grab an issue when there is only a Bitbucket PR reference in the subject
+        ("feat(parser): add magic parser (pull request #123)", []),
+    ],
+)
+def test_parser_return_linked_issues_from_commit_message(
+    default_angular_parser: AngularCommitParser,
+    message: str,
+    linked_issues: Sequence[str],
+    make_commit_obj: MakeCommitObjFn,
+):
+    result = default_angular_parser.parse(make_commit_obj(message))
+    assert isinstance(result, ParsedCommit)
+    assert tuple(linked_issues) == result.linked_issues
 
 
 ##############################
