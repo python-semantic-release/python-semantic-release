9632a81c6ca4115cfe2e9ec8830edc9725806d1c
codejedi365 <codejedi365@gmail.com>
2025-01-11 22:20:46 -0700

    chore(config): adjust build command & doc version bump script implementation

--- scripts/bump_version_in_docs.py
+++ scripts/bump_version_in_docs.py
@@ -3,35 +3,38 @@ from __future__ import annotations
 
 from os import getenv
 from pathlib import Path
-from re import compile as RegExp  # noqa: N812
+from re import compile as regexp
 
 PROJ_DIR = Path(__file__).resolve().parent.parent
 DOCS_DIR = PROJ_DIR / "docs"
 
 
-def update_github_actions_example(filepath: Path, new_version: str) -> None:
-    psr_regex = RegExp(r"(uses:.*python-semantic-release)@v\d+\.\d+\.\d+")
+def update_github_actions_example(filepath: Path, release_tag: str) -> None:
+    psr_regex = regexp(r"(uses: python-semantic-release/python-semantic-release)@\S+$")
+    psr_publish_action_regex = regexp(
+        r"(uses: python-semantic-release/publish-action)@\S+$"
+    )
     file_content_lines: list[str] = filepath.read_text().splitlines()
 
-    for regex in [psr_regex]:
+    for regex in [psr_regex, psr_publish_action_regex]:
         file_content_lines = list(
             map(
-                lambda line, regex=regex: regex.sub(r"\1@v" + new_version, line),  # type: ignore[misc]
+                lambda line, regex=regex: regex.sub(r"\1@" + release_tag, line),  # type: ignore[misc]
                 file_content_lines,
             )
         )
 
-    print(f"Bumping version in {filepath} to", new_version)
+    print(f"Bumping version in {filepath} to", release_tag)
     filepath.write_text(str.join("\n", file_content_lines) + "\n")
 
 
 if __name__ == "__main__":
-    new_version = getenv("NEW_VERSION")
+    new_release_tag = getenv("NEW_RELEASE_TAG")
 
-    if not new_version:
-        print("NEW_VERSION environment variable is not set")
+    if not new_release_tag:
+        print("NEW_RELEASE_TAG environment variable is not set")
         exit(1)
 
     update_github_actions_example(
-        DOCS_DIR / "automatic-releases" / "github-actions.rst", new_version
+        DOCS_DIR / "automatic-releases" / "github-actions.rst", new_release_tag
     )
