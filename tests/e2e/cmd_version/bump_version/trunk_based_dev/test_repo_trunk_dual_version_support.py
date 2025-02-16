a900b2b21318a8a59cdb25c3d99de732340b77bb
codejedi365 <codejedi365@gmail.com>
2025-02-16 16:23:17 -0700

    ci(tests-e2e): mark long running tests to prevent windows execution

--- tests/e2e/cmd_version/bump_version/trunk_based_dev/test_repo_trunk_dual_version_support.py
+++ tests/e2e/cmd_version/bump_version/trunk_based_dev/test_repo_trunk_dual_version_support.py
@@ -46,14 +46,12 @@ if TYPE_CHECKING:
 @pytest.mark.parametrize(
     "repo_fixture_name",
     [
-        repo_w_trunk_only_dual_version_spt_conventional_commits.__name__,
-        *[
-            pytest.param(repo_fixture_name, marks=pytest.mark.comprehensive)
-            for repo_fixture_name in [
-                repo_w_trunk_only_dual_version_spt_emoji_commits.__name__,
-                repo_w_trunk_only_dual_version_spt_scipy_commits.__name__,
-            ]
-        ],
+        pytest.param(repo_fixture_name, marks=pytest.mark.comprehensive)
+        for repo_fixture_name in [
+            repo_w_trunk_only_dual_version_spt_conventional_commits.__name__,
+            repo_w_trunk_only_dual_version_spt_emoji_commits.__name__,
+            repo_w_trunk_only_dual_version_spt_scipy_commits.__name__,
+        ]
     ],
 )
 def test_trunk_repo_rebuild_dual_version_spt_official_releases_only(
