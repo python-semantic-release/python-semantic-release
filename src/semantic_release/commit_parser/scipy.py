3cfee76032662bda6fbdd7e2585193213e4f9da2
codejedi365 <codejedi365@gmail.com>
2025-05-17 16:03:23 -0600

    fix(parser-scipy)!: remove issue footer messages from commit descriptions
    BREAKING CHANGE: Any issue resolution footers that the parser detects will now
    be removed from the commit.descriptions[] list. Previously, the descriptions included
    all text from the commit message but now that the parser pulls out the issue numbers
    the numbers will be included in the commit.linked_issues tuple for user extraction in
    any changelog generation.

--- src/semantic_release/commit_parser/scipy.py
+++ src/semantic_release/commit_parser/scipy.py
@@ -312,8 +312,7 @@ class ScipyCommitParser(CommitParser[ParseResult, ScipyParserOptions]):
                 accumulator["linked_issues"] = sort_numerically(
                     set(accumulator["linked_issues"]).union(new_issue_refs)
                 )
-                # TODO: breaking change v10, removes resolution footers from descriptions
-                # return accumulator
+                return accumulator
 
         # Prevent appending duplicate descriptions
         if text not in accumulator["descriptions"]:
