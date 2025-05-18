d31c4f417463c061f4f605c125b0d91e556a3ee4
codejedi365 <codejedi365@gmail.com>
2025-05-18 17:20:36 -0600

    feat(parser-emoji)!: set parser to ignore merge commits by default
    BREAKING CHANGE: The configuration setting `commit_parser_options.ignore_merge_commits`
    is now set to `true` by default. The feature to ignore squash commits was introduced in
    `v9.18.0` and was originally set to `false` to prevent unexpected results on a non-breaking
    update. The ignore merge commits feature prevents additional unnecessary processing on
    a commit message that likely will not match a commit message syntax. Most merge commits
    are syntactically pre-defined by Git or Remote Version Control System (ex. GitHub, etc.)
    and do not follow a commit convention (nor should they). The larger issue with merge commits
    is that they ultimately are a full copy of all the changes that were previously created
    and committed. The merge commit itself ensures that the previous commit tree is always
    existant therefore the commit message always exists. If merge commits are parsed, it
    generally creates duplicate messages that will end up in your changelog, which is less
    than desired in most cases. If you have previously used the `changelog.exclude_commit_patterns`
    functionality to ignore merge commit messages then you will want this setting set to `true`
    to improve parsing speed. You can also now remove the merge commit exclude pattern from the
    list as well to improve parsing speed. If this functionality is not desired, you will need to
    update your configuration to change the new setting to `false`.

--- src/semantic_release/commit_parser/emoji.py
+++ src/semantic_release/commit_parser/emoji.py
@@ -97,8 +97,7 @@ class EmojiParserOptions(ParserOptions):
     parse_squash_commits: bool = True
     """Toggle flag for whether or not to parse squash commits"""
 
-    # TODO: breaking change v10, change default to True
-    ignore_merge_commits: bool = False
+    ignore_merge_commits: bool = True
     """Toggle flag for whether or not to ignore merge commits"""
 
     @property
