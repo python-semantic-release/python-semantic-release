a65a72375f3bc228b008bc58edd903dcaebade37
codejedi365 <codejedi365@gmail.com>
2025-07-04 08:17:50 -0600

    test(gha): update e2e test for GitHub Actions output to include `commit_sha` output check
    ref: #717

--- tests/e2e/cmd_version/test_version_github_actions.py
+++ tests/e2e/cmd_version/test_version_github_actions.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
+from pytest_lazy_fixtures.lazy_fixture import lf as lazy_fixture
 
 from tests.const import MAIN_PROG_NAME, VERSION_SUBCMD
 from tests.fixtures.repos import (
@@ -13,16 +14,26 @@ from tests.util import actions_output_to_dict, assert_successful_exit_code
 if TYPE_CHECKING:
     from tests.conftest import RunCliFn
     from tests.fixtures.example_project import ExProjectDir
+    from tests.fixtures.git_repo import BuiltRepoResult
 
 
-@pytest.mark.usefixtures(
-    repo_w_git_flow_w_alpha_prereleases_n_conventional_commits.__name__
+@pytest.mark.parametrize(
+    "repo_result",
+    [lazy_fixture(repo_w_git_flow_w_alpha_prereleases_n_conventional_commits.__name__)],
 )
 def test_version_writes_github_actions_output(
+    repo_result: BuiltRepoResult,
     run_cli: RunCliFn,
     example_project_dir: ExProjectDir,
 ):
     mock_output_file = example_project_dir / "action.out"
+    expected_gha_output = {
+        "released": str(True).lower(),
+        "version": "1.2.1",
+        "tag": "v1.2.1",
+        "commit_sha": "0" * 40,
+        "is_prerelease": str(False).lower(),
+    }
 
     # Act
     cli_cmd = [MAIN_PROG_NAME, VERSION_SUBCMD, "--patch", "--no-push"]
@@ -30,6 +41,9 @@ def test_version_writes_github_actions_output(
         cli_cmd[1:], env={"GITHUB_OUTPUT": str(mock_output_file.resolve())}
     )
     assert_successful_exit_code(result, cli_cmd)
+
+    # Update the expected output with the commit SHA
+    expected_gha_output["commit_sha"] = repo_result["repo"].head.commit.hexsha
 
     if not mock_output_file.exists():
         pytest.fail(
@@ -42,9 +56,14 @@ def test_version_writes_github_actions_output(
     )
 
     # Evaluate
-    assert "released" in action_outputs
-    assert action_outputs["released"] == "true"
-    assert "version" in action_outputs
-    assert action_outputs["version"] == "1.2.1"
-    assert "tag" in action_outputs
-    assert action_outputs["tag"] == "v1.2.1"
+    expected_keys = set(expected_gha_output.keys())
+    actual_keys = set(action_outputs.keys())
+    key_difference = expected_keys.symmetric_difference(actual_keys)
+
+    assert not key_difference, f"Unexpected keys found: {key_difference}"
+
+    assert expected_gha_output["released"] == action_outputs["released"]
+    assert expected_gha_output["version"] == action_outputs["version"]
+    assert expected_gha_output["tag"] == action_outputs["tag"]
+    assert expected_gha_output["is_prerelease"] == action_outputs["is_prerelease"]
+    assert expected_gha_output["commit_sha"] == action_outputs["commit_sha"]
