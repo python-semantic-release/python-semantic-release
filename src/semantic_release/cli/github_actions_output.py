0bd81ace5bd49cc1b0b41e9ae49fa916003cb44e
codejedi365 <codejedi365@gmail.com>
2025-07-04 08:35:44 -0600

    feat(github-actions): add `commit_sha` as a GitHub Actions output value
    Implements: #717

--- src/semantic_release/cli/github_actions_output.py
+++ src/semantic_release/cli/github_actions_output.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from re import compile as regexp
 
 from semantic_release.globals import logger
 from semantic_release.version.version import Version
@@ -13,9 +14,11 @@ class VersionGitHubActionsOutput:
         self,
         released: bool | None = None,
         version: Version | None = None,
+        commit_sha: str | None = None,
     ) -> None:
         self._released = released
         self._version = version
+        self._commit_sha = commit_sha
 
     @property
     def released(self) -> bool | None:
@@ -45,12 +48,30 @@ class VersionGitHubActionsOutput:
     def is_prerelease(self) -> bool | None:
         return self.version.is_prerelease if self.version is not None else None
 
+    @property
+    def commit_sha(self) -> str | None:
+        return self._commit_sha if self._commit_sha else None
+
+    @commit_sha.setter
+    def commit_sha(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise TypeError("output 'commit_sha' should be a string")
+
+        if not regexp(r"^[0-9a-f]{40}$").match(value):
+            raise ValueError(
+                "output 'commit_sha' should be a valid 40-hex-character SHA"
+            )
+
+        self._commit_sha = value
+
     def to_output_text(self) -> str:
         missing = set()
         if self.version is None:
             missing.add("version")
         if self.released is None:
             missing.add("released")
+        if self.released and self.commit_sha is None:
+            missing.add("commit_sha")
 
         if missing:
             raise ValueError(
@@ -62,6 +83,7 @@ class VersionGitHubActionsOutput:
             "version": str(self.version),
             "tag": self.tag,
             "is_prerelease": str(self.is_prerelease).lower(),
+            "commit_sha": self.commit_sha if self.commit_sha else "",
         }
 
         return str.join("", [f"{key}={value!s}\n" for key, value in outputs.items()])
