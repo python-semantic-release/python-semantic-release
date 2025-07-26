7a9b96ccf61931bd3dd29f59ce67fad05103ad4b
codejedi365 <codejedi365@gmail.com>
2025-07-26 15:48:52 -0600

    test(gha): update e2e test for GitHub Actions output to include `link` output check
    ref: #512

--- tests/e2e/cmd_version/test_version_github_actions.py
+++ tests/e2e/cmd_version/test_version_github_actions.py
@@ -15,6 +15,8 @@ from tests.fixtures.repos import (
 from tests.util import actions_output_to_dict, assert_successful_exit_code
 
 if TYPE_CHECKING:
+    from semantic_release.hvcs.github import Github
+
     from tests.conftest import RunCliFn
     from tests.fixtures.example_project import ExProjectDir
     from tests.fixtures.git_repo import (
@@ -47,7 +49,7 @@ def test_version_writes_github_actions_output(
     all_versions = get_versions_from_repo_build_def(repo_def)
     latest_release_version = all_versions[-1]
     release_tag = tag_format_str.format(version=latest_release_version)
-
+    hvcs_client = cast("Github", get_hvcs_client_from_repo_def(repo_def))
     repo_actions_per_version = split_repo_actions_by_release_tags(
         repo_definition=repo_def,
         tag_format_str=tag_format_str,
@@ -56,13 +58,14 @@ def test_version_writes_github_actions_output(
         "released": str(True).lower(),
         "version": latest_release_version,
         "tag": release_tag,
+        "link": hvcs_client.create_release_url(release_tag),
         "commit_sha": "0" * 40,
         "is_prerelease": str(
             Version.parse(latest_release_version).is_prerelease
         ).lower(),
         "release_notes": generate_default_release_notes_from_def(
             version_actions=repo_actions_per_version[release_tag],
-            hvcs=get_hvcs_client_from_repo_def(repo_def),
+            hvcs=hvcs_client,
             previous_version=(
                 Version.parse(all_versions[-2]) if len(all_versions) > 1 else None
             ),
@@ -108,6 +111,7 @@ def test_version_writes_github_actions_output(
     assert expected_gha_output["version"] == action_outputs["version"]
     assert expected_gha_output["tag"] == action_outputs["tag"]
     assert expected_gha_output["is_prerelease"] == action_outputs["is_prerelease"]
+    assert expected_gha_output["link"] == action_outputs["link"]
     assert expected_gha_output["commit_sha"] == action_outputs["commit_sha"]
     assert (
         f'{expected_gha_output["release_notes"]}{os.linesep}'
