fe0f34b3b004c3f1007e5274346191feef2250af
codejedi365 <codejedi365@gmail.com>
2025-07-27 08:34:29 -0600

    refactor(parser-conventional): relocate parser file & separate options from parser class file

---
+++ src/semantic_release/commit_parser/conventional/__init__.py
@@ -0,0 +1,9 @@
+from semantic_release.commit_parser.conventional.options import (
+    ConventionalCommitParserOptions,
+)
+from semantic_release.commit_parser.conventional.parser import ConventionalCommitParser
+
+__all__ = [
+    "ConventionalCommitParser",
+    "ConventionalCommitParserOptions",
+]
