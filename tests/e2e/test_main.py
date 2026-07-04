e5ff73102c0153f9cc857b93d3eacd8576a7dd99
codejedi365 <codejedi365@users.noreply.github.com>
2026-07-04 13:25:30 -0600

    test: refactor e2e tests to work across `click@v8.1.0` to `v8.2.0`

--- tests/e2e/test_main.py
+++ tests/e2e/test_main.py
@@ -9,6 +9,7 @@ from typing import TYPE_CHECKING
 
 import git
 import pytest
+from click import __version__ as click_version
 from click.testing import CliRunner
 from pytest_lazy_fixtures.lazy_fixture import lf as lazy_fixture
 
@@ -65,7 +66,12 @@ def test_main_no_args_passes_w_help_text():
 
     cli_cmd = [MAIN_PROG_NAME]
     result = CliRunner().invoke(main, prog_name=cli_cmd[0])
-    assert_successful_exit_code(result, cli_cmd)
+
+    if tuple(map(int, click_version.split("."))) >= (8, 2, 0):
+        assert_exit_code(2, result, cli_cmd)
+    else:
+        assert_successful_exit_code(result, cli_cmd)
+
     assert "Usage: " in result.output
 
 
